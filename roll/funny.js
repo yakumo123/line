5e92d97e6f585568496e7e3b09fde6b91e32b158
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-08 00:00:52 +0800

    Update funny.js

--- roll/funny.js
+++ roll/funny.js
@@ -1,12 +1,12 @@
 var rollbase = require('./rollbase.js');
 var rply ={type : 'text'}; //type是必需的,但可以更改
 
-var HpNum = Math.ceil(Math.random()*10*5*9+Math.random()*10*8*3*7*102*Math.random()*Math.random())
-var MpNum = Math.ceil(Math.random()*10*50*93+Math.random()*10*8*34*7*72*Math.random()*Math.random()*0.7*0.1*Math.random())
+var = Math.ceil(Math.random()*10*5*9+Math.random()*10*8*3*7*102*Math.random()*Math.random()*0.01)
+var MpNum = Math.ceil(Math.random()*10*50*93+Math.random()*10*8*34*7*72*Math.random()*Math.random()*0.7*0.01*Math.random())
 
 function BStyleFlagSCRIPTS() {
 let PersonalDescriptionArr = ['神族', '魔族', '妖精族', '人族', '巨人族', '森精族', '獸人族', '天翼族', '機凱族', '魚人族', '吸血鬼族', '龍族']
-let IdeologyBeliefsArr = ['男性', '女性', '無性別'];
+let IdeologyBeliefsArr = ['男性', '女性'];
 let SignificantPeopleArr = ['結實的', '英俊的', '粗鄙的', '機靈的', '迷人的', '娃娃臉的', '聰明的', '蓬頭垢面的', '愚鈍的', '骯髒的', '耀眼的', '有書卷氣的', '青春洋溢的', '感覺疲憊的', '豐滿的', '粗壯的', '毛髮茂盛的', '苗條的', '優雅的', '邋遢的', '敦實的', '蒼白的', '陰沉的', '平庸的', '臉色紅潤的', '皮膚黝黑色', '滿臉皺紋的', '古板的', '有狐臭的', '狡猾的', '健壯的', '嬌俏的', '筋肉發達的', '魁梧的', '遲鈍的',  '虛弱的', '可愛的', '萌萌的', '帥氣的', '有獸耳的'];
 let SignificantPeopleWhyArr = ['虔誠信仰著某個神祈', '覺得人類不需要依靠宗教也可以好好生活', '覺得科學可以解釋所有事，並對某種科學領域有獨特的興趣', '相信因果循環與命運', '是一個政黨、社群或秘密結社的成員','覺得這個社會已經病了，而其中某些病灶需要被剷除', '是神秘學的信徒', '是積極參與政治的人，有特定的政治立場', '覺得金錢至上，且為了金錢不擇手段', '是一個激進主義分子，活躍於社會運動']
 let MeaningfulLocationsArr = ['他的父母', '他的祖父母', '他的兄弟姐妹', '他的孩子', '他的另一半','他的兒時好友', '他心目中的偶像或是英雄'];
