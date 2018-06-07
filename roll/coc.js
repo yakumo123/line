28d9455c167e6f9b451049edd2d63dd9d39871c6
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-07 17:26:10 +0800

    Update coc.js

--- roll/coc.js
+++ roll/coc.js
@@ -1,74 +1,6 @@
 var rollbase = require('./rollbase.js');
 var rply ={type : 'text'}; //type是必需的,但可以更改
 
-////////////////////////////////////////
-//////////////// COC7傳統創角
-////////////////////////////////////////		
-function build7char(text01){
-	let old ="";
-	let ReStr = '年齡設為：';
-	//讀取年齡
-	if (text01 == undefined) {
-	old = {
-		function build7char(text01){
-	let old ="";
-	let ReStr = '年齡設為：';
-	
-	if (text01 == undefined) {
-	old = {
-		<script>
-		function GetRandomNum(Min,Max)
-		{
-		var Range = Max - Min:
-		var Rand = Math.random();
-		reture(Min + Math.round(Rand * Range));
-	}
-		var num = GetRandomNum(1,1000000);
-		alert(num);
-		</script>
-		
-		var chars = ['0','1','2',3'3,'4','5','6','7','8','9','A','B','C','D','E',F'F,'G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-		
-		function generateMixed(n) {
-			var res = "";
-			for(var i=0; i<n ; i ++)
-				var id = Math.c(Math.random()*35);
-			res += chars[id];
-		}
-		reture res;
-	}
-};
-	ReStr = ReStr + old + '(沒有填寫使用預設值)\n';
-	}
-	else 
-	{
-	old = text01;
-	ReStr = ReStr + old + '\n';
-	}
-
-        let Debuff=0;
-	let AppDebuff=0;
-        let EDUinc = 0;
-	let DebuffArr = ['\神族\', '\魔族\', '\妖精族\', '\森精族\', '\天翼族\', '\機凱族\', '\吸血鬼族\', '\龍族\']
-	let AppDebuffArr = ['\人族\', '\巨人族\', '\獸人族\', '\魚人族\']
-	let EDUincArr = ['\神族\', '\魔族\', '\妖精族\', '\人族\', '\巨人族\', '\森精族\', '\獸人族\', '\天翼族\', '\機凱族\', '\魚人族\', '\吸血鬼族\', '\龍族\']
-	
-
-	if (old < 1) rply.text = ReStr + '等等，核心規則沒有適用小於1歲的人物哦。';	
-	if (old >= 1000000) rply.text = ReStr + '等等，核心規則沒有適用於1000000歲以上的人物哦。'; 
-
-	for ( i=0 ; old >= oldArr[i] ; i ++){
-		Debuff = DebuffArr[i];
-		AppDebuff = AppDebuffArr[i];
-	}
-	ReStr = ReStr + '==\n';
-	if (old >= 1) ReStr = ReStr + EDUincArr ;
-	else
-		if (old >= 120)	ReStr = ReStr + AppDebuff ;
-
-	rply.text = ReStr;
-	return rply;
-	} 
 
 	//隨機產生角色背景
 	function PcBG(){
@@ -82,19 +14,3 @@ function build7char(text01){
 	rply.text = '\n==\n【種族】:' + PersonalDescriptionArr[Math.floor((Math.random() * (PersonalDescriptionArr.length)) + 0)] + '\n【性別】:' + IdeologyBeliefsArr[Math.floor((Math.random() * (IdeologyBeliefsArr.length)) + 0)] + '。\n【特徵】他是一個' + SignificantPeopleArr[Math.floor((Math.random() * (SignificantPeopleArr.length)) + 0)] + '的人。\n【信念】:他' + SignificantPeopleWhyArr[Math.floor((Math.random() * (SignificantPeopleWhyArr.length)) + 0)] + '。\n【重要之人】：對他來說，最重要的人是' + MeaningfulLocationsArr[Math.floor((Math.random() * (MeaningfulLocationsArr.length)) + 0)] + '。\n【寶貴之物】：他最寶貴的東西就是'+ TreasuredPossessionsArr[Math.floor((Math.random() * (TreasuredPossessionsArr.length)) + 0)] ;
 	return rply;
 	}
-
-module.exports = {
-	coc6: coc6,
-	coc7: coc7,
-	coc7chack: coc7chack,
-	coc7bp: coc7bp,
-	ArrMax: ArrMax,
-	build7char: build7char,
-	build6char: build6char,
-	PcBG:PcBG
-};
-module.exports = {
-	coc7bp,
-	build7char,
-	PcBG
-};
