fd745681435ed2c91c52439e339f42e8ed43538a
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-07 17:41:36 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -19,41 +19,7 @@ function parseInput(rplyToken, inputStr) {
 	let mainMsg = inputStr.match(msgSplitor); //定義輸入字串
 	let trigger = mainMsg[0].toString().toLowerCase(); //指定啟動詞在第一個詞&把大階強制轉成細階
 
-	//在下面位置開始分析trigger
 
-	//普通ROLL擲骰判定在此	
-	if (inputStr.match(/\w/)!=null && inputStr.toLowerCase().match(/\d+d+\d/)!=null) return exports.rollbase.nomalDiceRoller(inputStr,mainMsg[0],mainMsg[1],mainMsg[2]);
-
-	//xBy>A 指令開始於此
-	if (trigger.match(/^(\d+)(b)(\d+)$/i)!= null) return exports.advroll.xBy(trigger,mainMsg[1],mainMsg[2]);
-	//xUy 指令開始於此	
-	if (trigger.match(/^(\d+)(u)(\d+)$/i)!= null && isNaN(mainMsg[1])== false) return exports.advroll.xUy(trigger,mainMsg[1],mainMsg[2],mainMsg[3]);
-	
-	if (trigger.match(/^ccb$|^cc$|^ccn[1-2]$|^cc[1-2]$/)!= null && mainMsg[1]<=1000 )
-	{		
-	//ccb指令開始於此	
-	if (trigger.match(/角色背景/)!= null ) return exports.coc.PcBG();
-  
-	//nc指令開始於此 來自Rainsting/TarotLineBot 
-	if (trigger.match(/^[1-4]n[c|a][+|-][1-99]$|^[1-4]n[c|a]$/)!= null ) return exports.nc.nechronica(trigger,mainMsg[1]);
-
-	//依戀
-	if (trigger.match(/(^nm$)/) != null)	return exports.nc.nechronica_mirenn(mainMsg[1]);
-
-	
-	//wod 指令開始於此
-	if (trigger.match(/^(\d+)(wd|wod)(\d|)((\+|-)(\d+)|)$/i)!= null)return exports.wod.wod(trigger,mainMsg[1]);
-
-	//Dx3 指令開始於此
-	if (trigger.match(/^(\d+)(dx)(\d|)(((\+|-)(\d+)|)((\+|-)(\d+)|))$/i)!= null)return exports.dx3.dx(trigger);
-
-	//Fisher–Yates shuffle
- 	//SortIt 指令開始於此
- 	if (trigger.match(/排序/)!= null && mainMsg.length >= 3) return exports.funny.SortIt(inputStr,mainMsg);
- 	if (trigger.match(/^d66$/)!= null ) return exports.advroll.d66(mainMsg[1]);
-	if (trigger.match(/^d66s$/)!= null ) return exports.advroll.d66s(mainMsg[1]);
-
-	
 	//choice 指令開始於此
 	if (trigger.match(/choice|隨機|選項|選1/)!= null && mainMsg.length >= 3) return exports.funny.choice(inputStr,mainMsg);
 	
@@ -66,6 +32,8 @@ function parseInput(rplyToken, inputStr) {
 	
 	//FLAG指令開始於此
 	if (trigger.match(/九雲/) != null) return exports.funny.BStyleFlagSCRIPTS() ;	
+	
+	if (trigger.match(/角色背景/)!= null) return exports.funny.PPCG() ;
 	
 	//鴨霸獸指令開始於此
 	if (trigger.match(/鴨霸獸|巴獸/) != null) return exports.funny.randomReply() ;	
