7b6b5207b41ea1d64f3f75bcb79b14a7991aac09
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-07 15:05:46 +0800

    Update coc.js

--- roll/coc.js
+++ roll/coc.js
@@ -120,83 +120,35 @@ function ArrMax (Arr){
 ////////////////////////////////////////		
 function build7char(text01){
 	let old ="";
-	let ReStr = '調查員年齡設為：';
-	//讀取年齡
+	let ReStr = '年齡設為：';
+	
 	if (text01 == undefined) {
-	old = 18;
-	ReStr = ReStr + old + '(沒有填寫使用預設值)\n';
+	old = {
+		<script>
+		function GetRandomNum(Min,Max)
+		{
+		var Range = Max - Min:
+		var Rand = Math.random();
+		reture(Min + Math.round(Rand * Range));
+	}
+		var num = GetRandomNum(1,900000000);
+		alert(num);
+		</script>
+		
+		var chars = ['0','1','2',3'3,'4','5','6','7','8','9','A','B','C','D','E',F'F,'G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+		
+		function generateMixed(n) {
+			var res = "";
+			for(var i=0; i<n ; i ++)
+				var id = Math.c(Math.random()*35);
+			res += chars[id];
+		}
+		reture res;
+	}
+}
+;
 	}
 	else 
-	{
-	old = text01;
-	ReStr = ReStr + old + '\n';
-	}
-	//設定 因年齡減少的點數 和 EDU加骰次數
-	let Debuff = 0;
-	let AppDebuff = 0;
-	let EDUinc = 0;
-	let oldArr = [15,20,40,50,60,70,80]
-	let DebuffArr = [5,0,5,10,20,40,80]
-	let AppDebuffArr = [0,0,5,10,15,20,25]
-	let EDUincArr = [0,1,2,3,4,4,4]
-
-	if (old < 15) rply.text = ReStr + '等等，核心規則沒有適用小於15歲的人物哦。';	
-	if (old >= 90) rply.text = ReStr + '等等，核心規則沒有適用於90歲以上的人物哦。'; 
-
-	for ( i=0 ; old >= oldArr[i] ; i ++){
-		Debuff = DebuffArr[i];
-		AppDebuff = AppDebuffArr[i];
-		EDUinc = EDUincArr[i];
-	}
-	ReStr = ReStr + '==\n';
-	if (old < 20) ReStr = ReStr + '年齡調整：從STR、SIZ擇一減去' + Debuff + '點\n（請自行手動選擇計算）。\n將EDU減去5點。LUK可擲兩次取高。' ;
-	else
-		if (old >= 40)	ReStr = ReStr + '年齡調整：從STR、CON或DEX中「總共」減去' + Debuff + '點\n（請自行手動選擇計算）。\n將APP減去' + AppDebuff +'點。可做' + EDUinc + '次EDU的成長擲骰。' ;
-	else ReStr = ReStr + '年齡調整：可做' + EDUinc + '次EDU的成長擲骰。' ;
-	ReStr = ReStr + '\n==';
-	if (old>=40) ReStr = ReStr + '\n（以下箭號三項，自選共減' + Debuff + '點。）' ;
-	if (old<20) ReStr = ReStr + '\n（以下箭號兩項，擇一減去' + Debuff + '點。）' ;
-	ReStr = ReStr + '\nＳＴＲ：' + rollbase.BuildDiceCal('3d6*5');
-	if (old>=40) ReStr = ReStr + ' ← 共減' + Debuff ;
-	if (old<20) ReStr = ReStr + ' ←擇一減' + Debuff ;
-	ReStr = ReStr + '\nＣＯＮ：' + rollbase.BuildDiceCal('3d6*5');
-	if (old>=40) ReStr = ReStr + ' ← 共減' + Debuff;
-	ReStr = ReStr + '\nＤＥＸ：' + rollbase.BuildDiceCal('3d6*5');
-	if (old>=40) ReStr = ReStr + ' ← 共減' + Debuff ;
-	if (old>=40) ReStr = ReStr + '\nＡＰＰ：' + rollbase.BuildDiceCal('3d6*5-' + AppDebuff);
-	else ReStr = ReStr + '\nＡＰＰ：' + rollbase.BuildDiceCal('3d6*5');
-	ReStr = ReStr + '\nＰＯＷ：' + rollbase.BuildDiceCal('3d6*5');
-	ReStr = ReStr + '\nＳＩＺ：' + rollbase.BuildDiceCal('(2d6+6)*5');
-	if (old<20) ReStr = ReStr + ' ←擇一減' + Debuff ;
-	ReStr = ReStr + '\nＩＮＴ：' + rollbase.BuildDiceCal('(2d6+6)*5');	
-	if (old<20) ReStr = ReStr + '\nＥＤＵ：' + rollbase.BuildDiceCal('3d6*5-5');
-	else {
-		let firstEDU = '(' + rollbase.BuildRollDice('2d6') + '+6)*5';
-		ReStr = ReStr + '\n==';
-		ReStr = ReStr + '\nＥＤＵ初始值：' + firstEDU + ' = ' + eval(firstEDU);
-		
-		let tempEDU = eval(firstEDU);
-
-		for (i = 1 ; i <= EDUinc ; i++){
-	let EDURoll = rollbase.Dice(100);
-	ReStr = ReStr + '\n第' + i + '次EDU成長 → ' + EDURoll;
-	if (EDURoll>tempEDU) {
-	let EDUplus = rollbase.Dice(10);
-	ReStr = ReStr + ' → 成長' + EDUplus +'點';
-	tempEDU = tempEDU + EDUplus;
-	}
-	else{
-	ReStr = ReStr + ' → 沒有成長';		
-	}
-		}
-		ReStr = ReStr + '\n';
-		ReStr = ReStr + '\nＥＤＵ最終值：' +tempEDU;
-	}
-	ReStr = ReStr + '\n==';
-
-	ReStr = ReStr + '\nＬＵＫ：' + rollbase.BuildDiceCal('3d6*5');	
-	if (old<20) ReStr = ReStr + '\nＬＵＫ加骰：' + rollbase.BuildDiceCal('3D6*5');
-
 
 	rply.text = ReStr;
 	return rply;
