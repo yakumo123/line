976898395deb94c5d175044a42fed4bb2be9b20a
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-08 09:57:28 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -7,57 +7,7 @@ require('fs').readdirSync('./roll/').forEach(function(file) {
 	}
   });
 
-//用來呼叫骰組,新增骰組的話,要寫條件式到下面呼叫 
-//格式是 exports.骰組檔案名字.function名
-function parseInput(rplyToken, inputStr) {
-	//console.log('InputStr: ' + inputStr);
-	_isNaN = function(obj) 	{
-	return isNaN(parseInt(obj));  
-	}
-	
-	let msgSplitor = (/\S+/ig);	
-	let mainMsg = inputStr.match(msgSplitor); //定義輸入字串
-	let trigger = mainMsg[0].toString().toLowerCase(); //指定啟動詞在第一個詞&把大階強制轉成細階
-
-
-	//Fisher–Yates shuffle
- 	//SortIt 指令開始於此
- 	if (trigger.match(/排序/)!= null && mainMsg.length >= 3) return exports.funny.SortIt(inputStr,mainMsg);
- 	if (trigger.match(/^d66$/)!= null ) return exports.advroll.d66(mainMsg[1]);
-	if (trigger.match(/^d66s$/)!= null ) return exports.advroll.d66s(mainMsg[1]);
-
-	
-	//choice 指令開始於此
-	if (trigger.match(/choice|隨機|選項|選1/)!= null && mainMsg.length >= 3) return exports.funny.choice(inputStr,mainMsg);
-	
-	
-	//tarot 指令
-	if (trigger.match(/tarot|塔羅牌|塔羅/) != null) {
-		if (trigger.match(/^單張|^每日|^daily/)!= null) return exports.funny.NomalDrawTarot(mainMsg[1], mainMsg[2]);//預設抽 79 張
-		if (trigger.match(/^時間|^time/)!= null) 	return exports.funny.MultiDrawTarot(mainMsg[1], mainMsg[2], 1);
-		if (trigger.match(/^大十字|^cross/)!= null) return exports.funny.MultiDrawTarot(mainMsg[1], mainMsg[2], 2);
-	}
-	
-	//FLAG指令開始於此
-	if (trigger.match(/角色背景/) != null) return exports.funny.BStyleFlagSCRIPTS() ;	
-	
-	//鴨霸獸指令開始於此
-	if (trigger.match(/女裝/) != null) return exports.funny.randomIIK() ;	
-	if (trigger.match(/運勢/) != null) return exports.funny.randomLuck(mainMsg) ; //占卜運氣		
-	
-
-	
-	/*tarot 指令
-	if (trigge// Load `*.js` under roll directory as properties
-//  i.e., `User.js` will become `exports['User']` or `exports.User`
-require('fs').readdirSync('./roll/').forEach(function(file) {
-	if (file.match(/\.js$/) !== null && file !== 'index.js') {
-	  var name = file.replace('.js', '');
-	  exports[name] = require('../roll/' + file);
-	}
-  });
-
-//用來呼叫骰組,新增骰組的話,要寫條件式到下面呼叫 
+//一呼叫骰組,新增骰組的話,要寫條件式到下面呼叫 
 //格式是 exports.骰組檔案名字.function名
 function parseInput(rplyToken, inputStr) {
 	//console.log('InputStr: ' + inputStr);
