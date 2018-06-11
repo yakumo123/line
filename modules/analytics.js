858e5b1b8d9b9c4e0798a4d68d72157b3859dac0
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-12 00:56:11 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -54,10 +54,7 @@ function parseInput(rplyToken, inputStr) {
 	if (trigger.match(/運勢|的機率/) != null) return exports.funny.randomLuck(mainMsg) ; //占卜運氣		
 	if (trigger.match(/鋪起來|鋪滿/) != null) return exports.funny.randomAOE() ;
 	
-	if (trigger.match(/codecommand5itv4ci42/) != null) return exports.funny.CommandRequire() ;
-	if (trigger.match(/CommandRequirementAns163/) != null) return exports.funny.CommandList() ;
-
-
+	
 	/*tarot 指令
 	if (trigger.match(/猜拳/) != null) {
 		return RockPaperScissors(inputStr, mainMsg[1]);
