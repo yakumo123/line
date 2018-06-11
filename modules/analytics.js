36fe8f5f35a988e0594982df9c5224b47dee8276
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-12 01:15:57 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -26,7 +26,7 @@ function parseInput(rplyToken, inputStr) {
 
 	if (trigger.match(/^式神Help$|^式神幫助$/)!= null ) return exports.help.Help();
 	
-	if (trigger.match(/^commandrequirementans96$/)!= null ) return exports.photo.CarPhoto();
+	if (trigger.match(/^js96$/)!= null ) return exports.photo.CarPhoto();
 
 	//Fisher–Yates shuffle
  	//SortIt 指令開始於此
