46df5c4361f7bb093afc895ffcaae458b50f8b7e
Yakumouou <40232503+Yakumouou@users.noreply.github.com>
2018-06-13 17:37:11 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -19,45 +19,7 @@ function parseInput(rplyToken, inputStr) {
 	let mainMsg = inputStr.match(msgSplitor); //定義輸入字串
 	let trigger = mainMsg[0].toString().toLowerCase(); //指定啟動詞在第一個詞&把大階強制轉成細階
 
-	//在下面位置開始分析trigger
-
-	//普通ROLL擲骰判定在此	
-	if (inputStr.match(/\w/)!=null && inputStr.toLowerCase().match(/\d+d+\d/)!=null) return exports.rollbase.nomalDiceRoller(inputStr,mainMsg[0],mainMsg[1],mainMsg[2]);
-
-	if (trigger.match(/^式神Help$|^式神幫助$/)!= null ) return exports.help.Help();
-	
-	if (trigger.match(/^commandrequirementans96$/)!= null ) return exports.security.Security();
-	
-	if (trigger.match(/^list0001$/)!= null ) return exports.photo.CarPhoto();
-	
-	if (trigger.match(/^list0002$/)!= null ) return exports.photo2.CarPhoto2();
-
-	//Fisher–Yates shuffle
- 	//SortIt 指令開始於此
- 	if (trigger.match(/排序|次序/)!= null && mainMsg.length >= 3) return exports.funny.SortIt(inputStr,mainMsg);
- 	if (trigger.match(/^d66$/)!= null ) return exports.advroll.d66(mainMsg[1]);
-	if (trigger.match(/^d66s$/)!= null ) return exports.advroll.d66s(mainMsg[1]);
-
-
-	//choice 指令開始於此
-	if (trigger.match(/choice|隨機|選項|選1/)!= null && mainMsg.length >= 3) return exports.funny.choice(inputStr,mainMsg);
-
-	//tarot 指令
-	if (trigger.match(/tarot|塔羅牌|塔羅/) != null) {
-		if (trigger.match(/^單張|^每日|^daily/)!= null) return exports.funny.NomalDrawTarot(mainMsg[1], mainMsg[2]);//預設抽 79 張
-		if (trigger.match(/^時間|^time/)!= null) 	return exports.funny.MultiDrawTarot(mainMsg[1], mainMsg[2], 1);
-		if (trigger.match(/^大十字|^cross/)!= null) return exports.funny.MultiDrawTarot(mainMsg[1], mainMsg[2], 2);
-	}
-
-	//FLAG指令開始於此
-	if (trigger.match(/角色背景/) != null) return exports.funny.BStyleFlagSCRIPTS() ;	
-
-	//鴨霸獸指令開始於此
-	if (trigger.match(/@吹雪/) != null) return exports.funny.randomYuki() ;	
-	if (trigger.match(/女裝/) != null) return exports.funny.randomReply() ;	
-	if (trigger.match(/運勢|的機率/) != null) return exports.funny.randomLuck(mainMsg) ; //占卜運氣		
-	if (trigger.match(/鋪起來|鋪滿/) != null) return exports.funny.randomAOE() ;	
-	
+	if (trigger.match(/^資訊$|^比賽$|^比賽資訊$|^比賽資料$|^資料$/)!= null ) return exports.help.Help();
 
 
 	/*tarot 指令
