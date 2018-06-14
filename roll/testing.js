819adee118b3f57aa5c873b3b88315956dd94534
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-15 00:09:15 +0800

    Update testing.js

--- roll/testing.js
+++ roll/testing.js
@@ -13,27 +13,27 @@ function Test12() {
             "imageUrl": "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg",
             "action": {
               "type": "datetimepicker",
-              "label": "選日期",
-              "data": "q1",
-              "mode": "date"
+              "label": "",
+              "data": "",
+              "mode": ""
             }
           },
           {
             "imageUrl": "https://www.cats.org.uk/uploads/images/featurebox_sidebar_kids/grief-and-loss.jpg",
             "action": {
               "type": "datetimepicker",
-              "label": "選時間",
-              "data": "q2",
-              "mode": "time"
+              "label": "",
+              "data": "",
+              "mode": ""
             }
           },
           {
             "imageUrl": "https://static.pexels.com/photos/126407/pexels-photo-126407.jpeg",
             "action": {
               "type": "datetimepicker",
-              "label": "選日期時間",
-              "data": "q3",
-              "mode": "datetime"
+              "label": "",
+              "data": "",
+              "mode": ""
             }
           }
 			]
