7c4b56975d975a36ddea1683d4daea4d4d81b23f
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-15 01:16:38 +0800

    Update flextest.js

--- roll/flextest.js
+++ roll/flextest.js
@@ -4,44 +4,35 @@ var rply = { type: 'text' }; //type是必需的,但可以更改
 
 function TestFlex() {
 	rply = {
-  "type": "bubble",
-  "header": {
-    "type": "box",
-    "layout": "vertical",
-    "contents": [
-      {
-        "type": "text",
-        "text": "Header text"
+  "type": "carousel",
+  "contents": [
+    {
+      "type": "bubble",
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": "First bubble"
+          }
+        ]
       }
-    ]
-  },
-  "hero": {
-    "type": "image",
-    "url": "https://1.bp.blogspot.com/-1mZFw01b99M/WyHcAmrVaiI/AAAAAAAAKqk/ryiMMs5Geag5oZfwAV8Tnby5A2udbUESgCLcBGAs/s200/005.png",
-  },
-  "body": {
-    "type": "box",
-    "layout": "vertical",
-    "contents": [
-      {
-        "type": "text",
-        "text": "Body text",
+    },
+    {
+      "type": "bubble",
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": "Second bubble"
+          }
+        ]
       }
-    ]
-  },
-  "footer": {
-    "type": "box",
-    "layout": "vertical",
-    "contents": [
-      {
-        "type": "text",
-        "text": "Footer text",
-      }
-    ]
-  },
-  "styles": {
-    "comment": "See the example of a bubble style object"
-  }
+    }
+  ]
 };
 	return rply;
 }
