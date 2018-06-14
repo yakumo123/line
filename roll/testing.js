27c377e36716a58cc04ab9a877b20eb71f278edc
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-14 23:45:51 +0800

    Create testing.js

---
+++ roll/testing.js
@@ -0,0 +1,18 @@
+{
+  "type": "template",
+  "altText": "this is a image carousel template",
+  "template": {
+      "type": "image_carousel",
+      "columns": [
+          {
+            "imageUrl": "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg",
+          },
+          {
+            "imageUrl": "https://www.cats.org.uk/uploads/images/featurebox_sidebar_kids/grief-and-loss.jpg",
+          },
+          {
+            "imageUrl": "https://static.pexels.com/photos/126407/pexels-photo-126407.jpeg",
+          }
+      ]
+  }
+}
