1237bc52a6e2afd7f9455f224a2dfd043ee4904f
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-15 00:17:55 +0800

    Update testing.js

--- roll/testing.js
+++ roll/testing.js
@@ -4,21 +4,22 @@ var rply = { type: 'text' }; //type是必需的,但可以更改
 
 function Test12() {
 	rply = {
-  "type": "bubble", // ①
-  "body": { // ②
-    "type": "box", // ③
-    "layout": "horizontal",　// ④
-    "contents": [ // ⑤
-      {
-        "type": "text", // ⑥
-        "text": "Hello,"
-      },
-      {
-        "type": "text", // ⑥
-        "text": "World!"
-      }
-    ]
-  }
+		"type": "bubble",
+		"body": {
+			"type": "box",
+			"layout": "horizontal",
+			"contents": [
+      
+				{
+					"type": "text",
+					"text": "Hello,"
+				},
+				{
+					"type": "text",
+					"text": "World!"
+				}
+			]
+		}
 	};
 	return rply;
 }
