8729dbaad2cff8c917a7c56b90b082d47d1c66c3
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-15 06:59:44 +0800

    Update flextest.js

--- roll/flextest.js
+++ roll/flextest.js
@@ -4,14 +4,29 @@ var rply = { type: 'text' }; //type是必需的,但可以更改
 
 function TestFlex() {
 	rply = {
-  "type": "button",
-  "action": {
-    "type": "uri",
-    "label": "Tap me",
-    "uri": "https://example.com"
-  }
-  "style": "primary",
-  "color": "#0000ff"
+  "richMenuId": "{richMenuId}",
+  "size": {
+    "width": 2500,
+    "height": 1686
+  },
+  "selected": false,
+  "name": "Nice richmenu",
+  "chatBarText": "Tap to open",
+  "areas": [
+    {
+      "bounds": {
+        "x": 0,
+        "y": 0,
+        "width": 2500,
+        "height": 1686
+      },
+      "action": {
+        "type": "postback",
+        "label":"Buy",
+        "data": "action=buy&itemid=123"
+      }
+    }
+  ]
 };
 	return rply;
 }
