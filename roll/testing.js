d2ae94856f762825d1489ad54e3cb85205d6eefc
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-14 23:49:35 +0800

    Update testing.js

--- roll/testing.js
+++ roll/testing.js
@@ -1,18 +1,26 @@
-{
-  "type": "template",
-  "altText": "this is a image carousel template",
-  "template": {
-      "type": "image_carousel",
-      "columns": [
-          {
-            "imageUrl": "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg",
-          },
-          {
-            "imageUrl": "https://www.cats.org.uk/uploads/images/featurebox_sidebar_kids/grief-and-loss.jpg",
-          },
-          {
-            "imageUrl": "https://static.pexels.com/photos/126407/pexels-photo-126407.jpeg",
-          }
-      ]
+var rollbase = require('./rollbase.js');
+var funny = require('./funny.js');
+var rply = { type: 'text' }; //type是必需的,但可以更改
+
+function Test12() {
+	rply = {
+      {
+    "type": "template",
+    "altText": "this is a image carousel template",
+    "template": {
+    "type": "image_carousel",
+    "columns": [
+    {
+    "imageUrl": "https://www.petfinder.com/wp-content/uploads/2012/11/91615172-find-a-lump-on-cats-skin-632x475.jpg",
+  },
+    {
+    "imageUrl": "https://www.cats.org.uk/uploads/images/featurebox_sidebar_kids/grief-and-loss.jpg",
+  },
+    {
+    "imageUrl": "https://static.pexels.com/photos/126407/pexels-photo-126407.jpeg",
   }
+  ]
 }
+}
+
+  
