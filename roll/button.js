806c34110bb74599db79e189ea58e47e7d5cc197
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-15 12:23:53 +0800

    Update button.js

--- roll/button.js
+++ roll/button.js
@@ -5,27 +5,27 @@ var rply = { type: 'text' }; //type是必需的,但可以更改
 function buttontest() {
 	rply = {
     "type": "template",
-    "altText": "Example buttons template",
+    "altText": "情報整合",
     "template": {
         "type": "buttons",
-        "thumbnailImageUrl": "https://api.reh.tw/line/bot/example/assets/images/example.jpg",
+        "thumbnailImageUrl": "https://i.ytimg.com/vi/G8O2TSRilkg/maxresdefault.jpg",
         "title": "Example Menu",
-        "text": "Please select",
+        "text": "所有情報",
         "actions": [
             {
-                "type": "postback",
-                "label": "Postback example",
-                "data": "action=buy&itemid=123"
-            },
-            {
-                "type": "message",
-                "label": "Message example",
-                "text": "Message example"
+                "type": "uri",
+                "label": "新卡包整合貼【創作者：LM】",
+                "uri": "https://m.gamer.com.tw/forum/C.php?bsn=29330&snA=14020&bpage=1&top=9&ltype="
             },
             {
                 "type": "uri",
-                "label": "Uri example",
-                "uri": "https://github.com/GoneTone/line-example-bot-php"
+                "label": "官方宣傳片",
+                "uri": "https://youtu.be/G8O2TSRilkg"
+            },
+            {
+                "type": "uri",
+                "label": "官網",
+                "uri": "https://shadowverse.com/sp/cht/cardpack/brigadeofthesky/"
             }
         ]
     }
