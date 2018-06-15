c9e6179365f1727b499e3eb3efc7278e8edd26c4
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-15 17:02:27 +0800

    Update and rename help.js to trpghelp.js

--- roll/trpghelp.js
+++ roll/trpghelp.js
@@ -2,7 +2,7 @@ var rollbase = require('./rollbase.js');
 var funny = require('./funny.js');
 var rply = { type: 'text' }; //type是必需的,但可以更改
 
-function Help() {
+function TRPG() {
 	rply = {
 		"type": "template",
 		"altText": "【擲骰BOT】v1.40 \
@@ -163,5 +163,5 @@ function Help() {
 
 
 module.exports = {
-	Help: Help
+	TRPG: TRPG
 };
