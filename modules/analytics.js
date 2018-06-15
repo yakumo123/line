0057b99e3c3c8e51f8fde10cfe448b9ee680d80d
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-15 15:19:53 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -64,7 +64,7 @@ function parseInput(rplyToken, inputStr) {
 
 	//鴨霸獸指令開始於此
 	if (trigger.match(/@吹雪/) != null) return exports.funny.randomYuki() ;	
-	if (trigger.match(/女裝/) != null) return exports.funny.randomReply() ;	
+	if (trigger.match(/^女裝$/) != null) return exports.funny.randomReply() ;	
 	if (trigger.match(/運勢|的機率是多少/) != null) return exports.funny.randomLuck(mainMsg) ; //占卜運氣		
 	if (trigger.match(/鋪起來|鋪滿/) != null) return exports.funny.randomAOE() ;	
 	
