1d230f0cb8b0cce66d137b296cd2b96951a13cf4
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-15 11:50:19 +0800

    Create button.js

---
+++ roll/button.js
@@ -0,0 +1,40 @@
+var rollbase = require('./rollbase.js');
+var funny = require('./funny.js');
+var rply = { type: 'text' }; //type是必需的,但可以更改
+
+function joke() {
+	rply = {
+    "type": "template",
+    "altText": "Example buttons template",
+    "template": {
+        "type": "buttons",
+        "thumbnailImageUrl": "https://api.reh.tw/line/bot/example/assets/images/example.jpg",
+        "title": "Example Menu",
+        "text": "Please select",
+        "actions": [
+            {
+                "type": "postback",
+                "label": "Postback example",
+                "data": "action=buy&itemid=123"
+            },
+            {
+                "type": "message",
+                "label": "Message example",
+                "text": "Message example"
+            },
+            {
+                "type": "uri",
+                "label": "Uri example",
+                "uri": "https://github.com/GoneTone/line-example-bot-php"
+            }
+        ]
+    }
+};
+	return rply;
+}
+
+
+
+module.exports = {
+	joke: joke
+};
