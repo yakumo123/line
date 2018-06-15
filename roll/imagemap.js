77192389f7e227b72a644613d5b984e9469f76b8
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-15 12:13:41 +0800

    Update imagemap.js

--- roll/imagemap.js
+++ roll/imagemap.js
@@ -5,8 +5,8 @@ var rply = { type: 'text' }; //type是必需的,但可以更改
 function immap() {
 	rply = {
     "type": "imagemap",
-    "baseUrl": "https://i.ytimg.com/vi/G8O2TSRilkg/maxresdefault.jpg",
-    "altText": "新卡包整合貼【創作者：LM】(左為新卡整合,右為宣傳影片)"
+    "baseUrl": "https://api.reh.tw/line/bot/example/assets/images/example",
+    "altText": "新卡包整合貼【創作者：LM】"
     "baseSize": {
         "height": 1040,
         "width": 1040
@@ -18,17 +18,7 @@ function immap() {
             "area": {
                 "x": 0,
                 "y": 0,
-                "width": 520,
-                "height": 1040
-            }
-        },
-        {
-            "type": "uri",
-            "linkUri": "https://youtu.be/G8O2TSRilkg",
-            "area": {
-                "x": 520,
-                "y": 0,
-                "width": 520,
+                "width": 1040,
                 "height": 1040
             }
         }
