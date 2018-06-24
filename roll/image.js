bb71302d3d35601490760711480d35649a42f3da
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-24 20:16:56 +0800

    Update image.js

--- roll/image.js
+++ roll/image.js
@@ -1,20 +1,25 @@
-{
-  "type": "template",
-  "altText": "this is a image carousel template",
-  "template": {
-      "type": "image_carousel",
+var rollbase = require('./rollbase.js');
+var funny = require('./funny.js');
+var rply = { type: 'text' }; //type是必需的,但可以更改
+
+function buttontest() {
+	rply = {
+    "type": "template",
+    "altText": "this is a image carousel template",
+    "template": {
+      "type": "image_carousel", 
       "columns": [
-          {
-            "imageUrl": "http://imgur.com/gallery/fwhOc79",
-            "action": {
-              "type": "message",
-              "label": "黑人問號",
-              "text": "黑人問號"
-            }
-          },
-          {
-            "imageUrl": "http://imgur.com/gallery/xBRN0dO",
-            "action": {
+        {
+          "imageUrl": "http://imgur.com/gallery/fwhOc79",
+          "action": {
+            "type": "message",
+            "label": "黑人問號",
+            "text": "黑人問號"
+          }
+        },
+        {   
+          "imageUrl": "http://imgur.com/gallery/xBRN0dO",
+          "action": {
               "type": "message",
               "label": "白人問號",
               "text": "白人問號"
