a829b07ed6e7ad74097cf76b30f98cb0af0d3f17
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-24 19:42:26 +0800

    Create image.js

---
+++ roll/image.js
@@ -0,0 +1,33 @@
+{
+  "type": "template",
+  "altText": "this is a image carousel template",
+  "template": {
+      "type": "image_carousel",
+      "columns": [
+          {
+            "imageUrl": "https://example.com/bot/images/item1.jpg",
+            "action": {
+              "type": "postback",
+              "label": "Buy",
+              "data": "action=buy&itemid=111"
+            }
+          },
+          {
+            "imageUrl": "https://example.com/bot/images/item2.jpg",
+            "action": {
+              "type": "message",
+              "label": "Yes",
+              "text": "yes"
+            }
+          },
+          {
+            "imageUrl": "https://example.com/bot/images/item3.jpg",
+            "action": {
+              "type": "uri",
+              "label": "View detail",
+              "uri": "http://example.com/page/222"
+            }
+          }
+      ]
+  }
+}
