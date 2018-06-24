06b4441ad06f6246b13571328950566187a4f3d5
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-24 20:25:59 +0800

    Update image.js

--- roll/image.js
+++ roll/image.js
@@ -4,29 +4,30 @@ var rply = { type: 'text' }; //type是必需的,但可以更改
 
 function questionMark() {
 	rply = {
-    "type": "template",
-    "altText": "this is a image carousel template",
-    "template": {
-      "type": "image_carousel", 
-      "columns": [
-        {
-          "imageUrl": "http://imgur.com/gallery/fwhOc79",
-          "action": {
-            "type": "message",
-            "label": "黑人問號",
-            "text": "黑人問號"
-          }
-        },
-        {   
-          "imageUrl": "http://imgur.com/gallery/xBRN0dO",
-          "action": {
-              "type": "message",
-              "label": "白人問號",
-              "text": "白人問號"
-	  }
-	}
-      ]
-};
+		"type": "template",
+		"altText": "this is a image carousel template",
+		"template": {
+			"type": "image_carousel", 
+			"columns": [
+				{
+					"imageUrl": "http://imgur.com/gallery/fwhOc79",
+					"action": {
+						"type": "message",
+						"label": "黑人問號",
+						"text": "黑人問號"
+					}
+				},
+				{   
+					"imageUrl": "http://imgur.com/gallery/xBRN0dO",
+					"action": {
+						"type": "message",
+						"label": "白人問號",
+						"text": "白人問號"
+					}
+				}
+				}
+
+				};
 	return rply;
 }
 
