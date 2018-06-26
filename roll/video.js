dcc67842039ee1a1e426bc7e67e0c18adabf53ba
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-26 10:57:58 +0800

    Update video.js

--- roll/video.js
+++ roll/video.js
@@ -5,7 +5,7 @@ var rply = { type: 'text' }; //type是必需的,但可以更改
 function mushroomcloud() {
 	rply = {
     "type": "video",
-    "originalContentUrl": "https://drive.google.com/uc?export=download&id=1HqrTGFbf-DLSHNi63jU72ugcmn0V-xJG",
+    "originalContentUrl": "https://drive.google.com/uc?export=download&id=1h7GhClMfoCxXCXdjVV8wiFylaMz-_IIk",
     "previewImageUrl": "https://i.kinja-img.com/gawker-media/image/upload/s--2Ps5CmqS--/c_fill,fl_progressive,g_center,h_900,q_80,w_1600/1813aqov5wc2jjpg.jpg"
 };
 	return rply;
