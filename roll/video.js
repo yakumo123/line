4542253cdd5cfe393ab5d72afb2430c5a29539be
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-26 10:12:06 +0800

    Update video.js

--- roll/video.js
+++ roll/video.js
@@ -5,7 +5,7 @@ var rply = { type: 'text' }; //type是必需的,但可以更改
 function mushroomcloud() {
 	rply = {
     "type": "video",
-    "originalContentUrl": "https://streamable.com/h0fdd",
+    "originalContentUrl": "https://drive.google.com/uc?export=download&id=1HqrTGFbf-DLSHNi63jU72ugcmn0V-xJG",
     "previewImageUrl": "https://i.imgur.com/5zrj83B.png"
 };
 	return rply;
