4bc857ad4c002474efc3d6f1fcc70e6668d40349
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-26 10:15:22 +0800

    Update video.js

--- roll/video.js
+++ roll/video.js
@@ -6,7 +6,7 @@ function mushroomcloud() {
 	rply = {
     "type": "video",
     "originalContentUrl": "https://drive.google.com/uc?export=download&id=1HqrTGFbf-DLSHNi63jU72ugcmn0V-xJG",
-    "previewImageUrl": "https://i.imgur.com/5zrj83B.png"
+    "previewImageUrl": "https://i.kinja-img.com/gawker-media/image/upload/s--2Ps5CmqS--/c_fill,fl_progressive,g_center,h_900,q_80,w_1600/1813aqov5wc2jjpg.jpg"
 };
 	return rply;
 }
