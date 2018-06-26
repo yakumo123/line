1a6bbcfbfb19508c79d6dbeacbfc006df20d8c74
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-26 09:34:08 +0800

    Update imagetwo.js

--- roll/imagetwo.js
+++ roll/imagetwo.js
@@ -4,15 +4,19 @@ var rply = { type: 'text' }; //type是必需的,但可以更改
 
 function immsnow() {
 	rply = {
-    "type": "image",
-    "originalContentUrl": "https://i.imgur.com/Q4JrEWq.jpg",
-    "previewImageUrl": "https://i.imgur.com/Q4JrEWq.jpg"
-},
-{
-    "type": "image",
-    "originalContentUrl": "https://i.imgur.com/h6sJiS9.jpg",
-    "previewImageUrl": "https://i.imgur.com/h6sJiS9.jpg"
-};
+		"messages":[
+			{
+				"type": "image",
+				"originalContentUrl": "https://i.imgur.com/Q4JrEWq.jpg",
+				"previewImageUrl": "https://i.imgur.com/Q4JrEWq.jpg"
+			},
+			{
+				"type": "image",
+				"originalContentUrl": "https://i.imgur.com/h6sJiS9.jpg",
+				"previewImageUrl": "https://i.imgur.com/h6sJiS9.jpg"
+			}
+		]
+	};
 	return rply;
 }
 
