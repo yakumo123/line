d0634b3a631e1c67becbc779df9affa6b54bd991
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-26 11:48:24 +0800

    Update imagepierr.js

--- roll/imagepierr.js
+++ roll/imagepierr.js
@@ -1,1 +1,18 @@
-y
+var rollbase = require('./rollbase.js');
+var funny = require('./funny.js');
+var rply = { type: 'text' }; //type是必需的,但可以更改
+
+function PieImm() {
+	rply = {
+    "type": "image",
+    "originalContentUrl": "https://i.imgur.com/piKDISn.jpg",
+    "previewImageUrl": "https://i.imgur.com/piKDISn.jpg"
+};
+	return rply;
+}
+
+
+
+module.exports = {
+	PieImm: PieImm
+};
