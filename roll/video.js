0526b801028535a6a9fd7d0b90ae5ca00054251d
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-26 10:42:41 +0800

    Update video.js

--- roll/video.js
+++ roll/video.js
@@ -5,12 +5,11 @@ var rply = { type: 'text' }; //type是必需的,但可以更改
 function song() {
 	rply = {
     "type": "video",
-    "originalContentUrl": "https://drive.google.com/uc?export=download&id=1cKIrT_lqcb76bHNRjdWVflUoxAMj76BL",
+    "originalContentUrl": "https://drive.google.com/uc?export=download&id=1h7GhClMfoCxXCXdjVV8wiFylaMz-_IIk",
     "previewImageUrl": "http://i.imgur.com/Y1w9t.jpg"
 };
 	return rply;
 }
-
 
 
 module.exports = {
