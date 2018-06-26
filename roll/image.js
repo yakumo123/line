dbfaeb66cd0a371ad2933a9ba646406ed201186b
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-26 08:51:09 +0800

    Update image.js

--- roll/image.js
+++ roll/image.js
@@ -1,4 +1,9 @@
-{
+var rollbase = require('./rollbase.js');
+var funny = require('./funny.js');
+var rply = { type: 'text' }; //type是必需的,但可以更改
+
+function imsnow() {
+	rply = {
     "type": "image",
     "originalContentUrl": "https://example.com/original.jpg",
     "previewImageUrl": "https://example.com/preview.jpg"
