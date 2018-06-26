ad9231f6ae2222c0495f87c13e3edce7d6e1449a
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-26 10:53:43 +0800

    Update video.js

--- roll/video.js
+++ roll/video.js
@@ -2,16 +2,17 @@ var rollbase = require('./rollbase.js');
 var funny = require('./funny.js');
 var rply = { type: 'text' }; //type是必需的,但可以更改
 
-function immsong() {
+function mushroomcloud() {
 	rply = {
     "type": "video",
-    "originalContentUrl": "https://drive.google.com/uc?export=download&id=1h7GhClMfoCxXCXdjVV8wiFylaMz-_IIk",
-    "previewImageUrl": "http://i.imgur.com/Y1w9t.jpg"
+    "originalContentUrl": "https://drive.google.com/uc?export=download&id=1HqrTGFbf-DLSHNi63jU72ugcmn0V-xJG",
+    "previewImageUrl": "https://i.kinja-img.com/gawker-media/image/upload/s--2Ps5CmqS--/c_fill,fl_progressive,g_center,h_900,q_80,w_1600/1813aqov5wc2jjpg.jpg"
 };
 	return rply;
 }
 
 
+
 module.exports = {
-	immsong: immsong
+	mushroomcloud: mushroomcloud
 };
