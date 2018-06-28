2f6b9860c2c7dfbcc6469dcb61489741925d0f46
annie123rc <40669684+annie123rc@users.noreply.github.com>
2018-06-29 07:08:03 +0800

    Add files via upload

---
+++ roll/button.js
@@ -0,0 +1,40 @@
+var rollbase = require('./rollbase.js');
+var funny = require('./funny.js');
+var rply = { type: 'text' }; //type是必需的,但可以更改
+
+function buttontest() {
+	rply = {
+    "type": "template",
+    "altText": "情報整合",
+    "template": {
+        "type": "buttons",
+        "thumbnailImageUrl": "https://i.ytimg.com/vi/G8O2TSRilkg/maxresdefault.jpg",
+        "title": "新卡包情報",
+        "text": "所有情報",
+        "actions": [
+            {
+                "type": "uri",
+                "label": "新卡包整合貼【創作者：LM】",
+                "uri": "https://m.gamer.com.tw/forum/C.php?bsn=29330&snA=14020&bpage=1&top=9&ltype="
+            },
+            {
+                "type": "uri",
+                "label": "官方宣傳片",
+                "uri": "https://youtu.be/G8O2TSRilkg"
+            },
+            {
+                "type": "uri",
+                "label": "官網",
+                "uri": "https://shadowverse.com/sp/cht/cardpack/brigadeofthesky/"
+            }
+        ]
+    }
+};
+	return rply;
+}
+
+
+
+module.exports = {
+	buttontest: buttontest
+};
