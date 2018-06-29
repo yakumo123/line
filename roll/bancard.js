f59c865bbee375fd8a366a66961f05f0e7914cda
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-30 07:55:23 +0800

    Create bancard.js

---
+++ roll/bancard.js
@@ -0,0 +1,199 @@
+function MultiDrawTarot(CardToCal, text, type) {
+	let returnStr = '';
+	var tmpcard = 0;
+	var cards = [];
+	var revs = [];
+	var i = 0;
+
+	if (type == 1) //時間之流
+	{
+	cards[0] = rollbase.FunnyDice(79); //先抽第0張
+	revs[0] = rollbase.FunnyDice(2);
+
+	for (i = 1; i < 3; i++) {
+		for (;;) {
+		tmpcard = rollbase.FunnyDice(79);
+		if (cards.indexOf(tmpcard) === -1) //沒有重複，就這張了
+		{
+			cards.push(tmpcard);
+			revs[i] = rollbase.FunnyDice(2);
+			break;
+		}
+		}
+	}
+
+	if (text != null)
+		returnStr += text + ': \n';
+
+	for (i = 0; i < 3; i++) {
+		if (i == 0) returnStr += '過去: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+		if (i == 1) returnStr += '現在: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+		if (i == 2) returnStr += '未來: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]);
+	}
+
+	} else if (type == 2) //塞爾特大十字
+	{
+	cards[0] = rollbase.FunnyDice(79); //先抽第0張
+	revs[0] = rollbase.FunnyDice(2);
+
+	for (i = 1; i < 10; i++) {
+		for (;;) {
+		tmpcard = rollbase.FunnyDice(79);
+		if (cards.indexOf(tmpcard) === -1) //沒有重複，就這張了
+		{
+			cards.push(tmpcard);
+			revs[i] = rollbase.FunnyDice(2);
+			break;
+		}
+		}
+	}
+
+	if (text != null)
+		returnStr += text + ': \n';
+
+	for (i = 0; i < 10; i++) {
+		if (i == 0) returnStr += '現況: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+		if (i == 1) {
+		if (revs[i] == 0) //正位
+			returnStr += '助力: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+		else
+			returnStr += '阻力: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+		}
+		if (i == 2) returnStr += '目標: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+		if (i == 3) returnStr += '基礎: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+		if (i == 4) returnStr += '過去: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+		if (i == 5) returnStr += '未來: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+		if (i == 6) returnStr += '自我: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+		if (i == 7) returnStr += '環境: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+		if (i == 8) {
+		if (revs[i] == 0) //正位
+			returnStr += '希望: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+		else
+			returnStr += '恐懼: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]) + '\n';
+		}
+		if (i == 9) returnStr += '結論: ' + tarotCardReply(cards[i]) + ' ' + tarotRevReply(revs[i]);
+
+	}
+
+	} else {
+
+	if (text == null)
+		returnStr = tarotCardReply(rollbase.FunnyDice(79)) + ' ' + tarotRevReply(rollbase.FunnyDice(2));
+	else
+		returnStr = tarotCardReply(rollbase.FunnyDice(79)) + ' ' + tarotRevReply(rollbase.FunnyDice(2)) + ' ; ' + text;
+	}
+
+
+	rply.text = returnStr;
+	return rply;
+}
+
+function NomalDrawTarot(CardToCal, text) {
+	let returnStr = '';
+
+	if (text == null)
+	returnStr = tarotCardReply(rollbase.FunnyDice(22)) + ' ' + tarotRevReply(rollbase.FunnyDice(2));
+	else
+	returnStr = tarotCardReply(rollbase.FunnyDice(22)) + ' ' + tarotRevReply(rollbase.FunnyDice(2)) + ' ; ' + text;
+	rply.text = returnStr;
+	return rply;
+}
+
+
+function tarotRevReply(count) {
+	let returnStr = '';
+
+	if (count == 0) returnStr = '＋';
+	if (count == 1) returnStr = '－';
+
+	return returnStr;
+	//return rply;
+}
+
+
+function tarotCardReply(count) {
+	let returnStr = '';
+	// returnStr = count + '愚者';
+	if (count == 0) returnStr = '愚者';
+	if (count == 1) returnStr = '魔術師';
+	if (count == 2) returnStr = '女祭司';
+	if (count == 3) returnStr = '女皇';
+	if (count == 4) returnStr = '皇帝';
+	if (count == 5) returnStr = '教皇';
+	if (count == 6) returnStr = '戀人';
+	if (count == 7) returnStr = '戰車';
+	if (count == 8) returnStr = '力量';
+	if (count == 9) returnStr = '隱者';
+	if (count == 10) returnStr = '命運之輪';
+	if (count == 11) returnStr = '正義';
+	if (count == 12) returnStr = '吊人';
+	if (count == 13) returnStr = '死神';
+	if (count == 14) returnStr = '節制';
+	if (count == 15) returnStr = '惡魔';
+	if (count == 16) returnStr = '高塔';
+	if (count == 17) returnStr = '星星';
+	if (count == 18) returnStr = '月亮';
+	if (count == 19) returnStr = '太陽';
+	if (count == 20) returnStr = '審判';
+	if (count == 21) returnStr = '世界';
+	if (count == 22) returnStr = '權杖一';
+	if (count == 23) returnStr = '權杖二';
+	if (count == 24) returnStr = '權杖三';
+	if (count == 25) returnStr = '權杖四';
+	if (count == 26) returnStr = '權杖五';
+	if (count == 27) returnStr = '權杖六';
+	if (count == 28) returnStr = '權杖七';
+	if (count == 29) returnStr = '權杖八';
+	if (count == 30) returnStr = '權杖九';
+	if (count == 31) returnStr = '權杖十';
+	if (count == 32) returnStr = '權杖侍者';
+	if (count == 33) returnStr = '權杖騎士';
+	if (count == 34) returnStr = '權杖皇后';
+	if (count == 35) returnStr = '權杖國王';
+	if (count == 36) returnStr = '聖杯一';
+	if (count == 37) returnStr = '聖杯二';
+	if (count == 38) returnStr = '聖杯三';
+	if (count == 39) returnStr = '聖杯四';
+	if (count == 40) returnStr = '聖杯五';
+	if (count == 41) returnStr = '聖杯六';
+	if (count == 42) returnStr = '聖杯七';
+	if (count == 43) returnStr = '聖杯八';
+	if (count == 44) returnStr = '聖杯九';
+	if (count == 45) returnStr = '聖杯十';
+	if (count == 46) returnStr = '聖杯侍者';
+	if (count == 47) returnStr = '聖杯騎士';
+	if (count == 48) returnStr = '聖杯皇后';
+	if (count == 49) returnStr = '聖杯國王';
+	if (count == 50) returnStr = '寶劍一';
+	if (count == 51) returnStr = '寶劍二';
+	if (count == 52) returnStr = '寶劍三';
+	if (count == 53) returnStr = '寶劍四';
+	if (count == 54) returnStr = '寶劍五';
+	if (count == 55) returnStr = '寶劍六';
+	if (count == 56) returnStr = '寶劍七';
+	if (count == 57) returnStr = '寶劍八';
+	if (count == 58) returnStr = '寶劍九';
+	if (count == 59) returnStr = '寶劍十';
+	if (count == 60) returnStr = '寶劍侍者';
+	if (count == 61) returnStr = '寶劍騎士';
+	if (count == 62) returnStr = '寶劍皇后';
+	if (count == 63) returnStr = '寶劍國王';
+	if (count == 64) returnStr = '錢幣一';
+	if (count == 65) returnStr = '錢幣二';
+	if (count == 66) returnStr = '錢幣三';
+	if (count == 67) returnStr = '錢幣四';
+	if (count == 68) returnStr = '錢幣五';
+	if (count == 69) returnStr = '錢幣六';
+	if (count == 70) returnStr = '錢幣七';
+	if (count == 71) returnStr = '錢幣八';
+	if (count == 72) returnStr = '錢幣九';
+	if (count == 73) returnStr = '錢幣十';
+	if (count == 74) returnStr = '錢幣侍者';
+	if (count == 75) returnStr = '錢幣騎士';
+	if (count == 76) returnStr = '錢幣皇后';
+	if (count == 77) returnStr = '錢幣國王';
+	if (count == 78) returnStr = '空白牌';
+	return returnStr;
+	//return rply;
+
+}
