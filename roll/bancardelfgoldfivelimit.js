c6f584da37dd2ccaec2459af246e5c9e0c5f6ae0
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-06-30 23:47:22 +0800

    Create bancardelfgoldfivelimit.js

---
+++ roll/bancardelfgoldfivelimit.js
@@ -0,0 +1,140 @@
+
+var rollbase = require('./rollbase.js');
+var rply ={type : 'text'}; //type是必需的,但可以更改
+
+
+function MultiDrawBan(CardToCal, text, type) {
+	let returnStr = '';
+	var tmpcard = 0;
+	var cards = [];
+	var revs = [];
+	var i = 0;
+
+	if (type == 1) 
+	{
+	cards[0] = rollbase.FunnyDice(14); 
+	revs[0] = rollbase.FunnyDice(2);
+
+	for (i = 1; i < 3; i++) {
+		for (;;) {
+		tmpcard = rollbase.FunnyDice(14);
+		if (cards.indexOf(tmpcard) === -1) 
+		{
+			cards.push(tmpcard);
+			revs[i] = rollbase.FunnyDice(2);
+			break;
+		}
+		}
+	}
+
+	if (text != null)
+		returnStr += text + ': \n';
+
+	for (i = 0; i < 3; i++) {
+		if (i == 0) returnStr += '1: ' + BanCardReply(cards[i]) + ' ' + BanRevReply(revs[i]) + '\n';
+		if (i == 1) returnStr += '1: ' + BanCardReply(cards[i]) + ' ' + BanRevReply(revs[i]) + '\n';
+		if (i == 2) returnStr += '1: ' + BanCardReply(cards[i]) + ' ' + BanRevReply(revs[i]);
+	}
+
+	} else if (type == 2) 
+	{
+	cards[0] = rollbase.FunnyDice(14);
+	revs[0] = rollbase.FunnyDice(2);
+
+		for (i = 1; i < 5; i++) {
+			for (;;) {
+			tmpcard = rollbase.FunnyDice(10);
+			if (cards.indexOf(tmpcard) === -1) //沒有重複，就這張了
+			{
+				cards.push(tmpcard);
+				revs[i] = rollbase.FunnyDice(2);
+				break;
+			}
+			}
+		}
+	
+
+		if (text != null)
+			returnStr += text + ': \n';
+	
+
+		for (i = 0; i < 5; i++) {
+			if (i == 0) returnStr += '1: ' + BanCardReply(cards[i]) + ' ' + '\n';
+			if (i == 1) {
+			if (revs[i] == 0)
+				returnStr += '2: ' + BanCardReply(cards[i]) + ' ' + '\n';
+			else
+				returnStr += '2: ' + BanCardReply(cards[i]) + ' ' + '\n';
+			}
+			if (i == 2) returnStr += '3: ' + BanCardReply(cards[i]) + ' ' + '\n';
+			if (i == 3) {
+			if (revs[i] == 0) 
+				returnStr += '4: ' + BanCardReply(cards[i]) + ' ' + '\n';
+			else
+				returnStr += '4: ' + BanCardReply(cards[i]) + ' ' + '\n';
+			}
+			if (i == 4) returnStr += '5: ' + BanCardReply(cards[i]) + ' ' ;
+	
+
+		}
+
+
+	} else {
+
+	if (text == null)
+		returnStr = BanCardReply(rollbase.FunnyDice(14)) + ' ' ;
+	else
+		returnStr = BanCardReply(rollbase.FunnyDice(14)) + ' ' + ' ; ' + text;
+	}
+
+
+	rply.text = returnStr;
+	return rply;
+}
+
+
+function NomalDrawBan(CardToCal, text) {
+	let returnStr = '';
+
+	if (text == null)
+	returnStr = BanCardReply(rollbase.FunnyDice(22)) + ' ' ;
+	else
+	returnStr = BanCardReply(rollbase.FunnyDice(22)) + ' ' + ' ; ' + text;
+	rply.text = returnStr;
+	return rply;
+}
+
+function BanCardReply(count) {
+	let returnStr = '';
+	if (count == 0) returnStr = '妖精劍士-金';
+	if (count == 1) returnStr = '瞬速的巨狼-金';
+	if (count == 2) returnStr = '維納斯-金';
+	if (count == 3) returnStr = '天賦的射手‧梅提拉-金';
+	if (count == 4) returnStr = '妖精使役者-金';
+	if (count == 5) returnStr = '高貴的貓姊妹‧暹羅與暹瑪-金';
+	if (count == 6) returnStr = '魔族少年‧翟恩-金';
+	if (count == 7) returnStr = '妖精大魔導士‧莉拉-金';
+	if (count == 8) returnStr = '愛之妖精‧寶菈-金';
+	if (count == 9) returnStr = '歷戰的馴鷹師-金';
+	if (count == 10) returnStr = '精靈的雙擊-金';
+	if (count == 11) returnStr = '精靈鍛造師‧蕾妮-金';
+	if (count == 12) returnStr = '新綠的加護-金';
+	if (count == 13) returnStr = '妖精的奔流-金';
+
+
+
+
+
+
+
+	return returnStr;
+	//return rply;
+
+}
+
+
+module.exports = {
+	MultiDrawBan,
+	NomalDrawBan,
+	BanCardReply	
+};
