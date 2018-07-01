799dad65492c93086160b37b41be7f2e5bb3bdaa
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-07-01 13:35:59 +0800

    Update botyakumo.js

--- roll/botyakumo.js
+++ roll/botyakumo.js
@@ -11,7 +11,7 @@ function BotHelpYy() {
 			"columns": [
 				{
 					"title": "《更新詳情》",
-					"text": "部份功能優化，簡單化及修正，新增查詢新卡包功能",
+					"text": "新增特殊賽制功能",
 					"actions": [
 						{
 							"type": "message",
