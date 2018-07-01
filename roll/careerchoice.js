b18ee04a5b6271573dd40b4553e38f69019ed7e1
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-07-01 12:15:13 +0800

    Create careerchoice.js

---
+++ roll/careerchoice.js
@@ -0,0 +1,122 @@
+
+var rollbase = require('./rollbase.js');
+var rply ={type : 'text'}; //type是必需的,但可以更改
+
+
+function MultiDrawBan(CardToCal, text, type) {
+	let returnStr = '';
+	var tmpcard = 0;
+	var cards = [];
+	var revs = [];
+	var i = 0;
+
+	if (type == 1) 
+	{
+	cards[0] = rollbase.FunnyDice(8); 
+	revs[0] = rollbase.FunnyDice(2);
+
+	for (i = 1; i < 3; i++) {
+		for (;;) {
+		tmpcard = rollbase.FunnyDice(8);
+		if (cards.indexOf(tmpcard) === -1) 
+		{
+			cards.push(tmpcard);
+			revs[i] = rollbase.FunnyDice(2);
+			break;
+		}
+		}
+	}
+
+	if (text != null)
+		returnStr += text + ': \n';
+
+	for (i = 0; i < 3; i++) {
+		if (i == 0) returnStr += '1: ' + BanCardReply(cards[i]) + ' ' + BanRevReply(revs[i]) + '\n';
+		if (i == 1) returnStr += '1: ' + BanCardReply(cards[i]) + ' ' + BanRevReply(revs[i]) + '\n';
+		if (i == 2) returnStr += '1: ' + BanCardReply(cards[i]) + ' ' + BanRevReply(revs[i]);
+	}
+
+	} else if (type == 2) 
+	{
+	cards[0] = rollbase.FunnyDice(8);
+	revs[0] = rollbase.FunnyDice(2);
+
+	for (i = 1; i < 3; i++) {
+		for (;;) {
+		tmpcard = rollbase.FunnyDice(8);
+		if (cards.indexOf(tmpcard) === -1) //沒有重複，就這張了
+		{
+			cards.push(tmpcard);
+			revs[i] = rollbase.FunnyDice(2);
+			break;
+		}
+		}
+	}
+
+	if (text != null)
+		returnStr += text + ': \n';
+
+	for (i = 0; i < 3; i++) {
+		if (i == 0) returnStr += '【隨機職業建議】 \n 第1: ' + BanCardReply(cards[i]) + ' ' + '\n';
+		if (i == 1) {
+		if (revs[i] == 0) 
+			returnStr += '第2: ' + BanCardReply(cards[i]) + ' ' + '\n';
+		else
+			returnStr += '第2: ' + BanCardReply(cards[i]) + ' ' + '\n';
+		}
+		if (i == 2) returnStr += '第3: ' + BanCardReply(cards[i]) + ' ' ;
+
+	}
+
+	} else {
+
+	if (text == null)
+		returnStr = BanCardReply(rollbase.FunnyDice(8)) + ' ' ;
+	else
+		returnStr = BanCardReply(rollbase.FunnyDice(8)) + ' ' + ' ; ' + text;
+	}
+
+
+	rply.text = returnStr;
+	return rply;
+}
+
+
+function NomalDrawBan(CardToCal, text) {
+	let returnStr = '';
+
+	if (text == null)
+	returnStr = BanCardReply(rollbase.FunnyDice(22)) + ' ' ;
+	else
+	returnStr = BanCardReply(rollbase.FunnyDice(22)) + ' ' + ' ; ' + text;
+	rply.text = returnStr;
+	return rply;
+}
+
+function BanCatdReply(count) {
+	let returnStr = '';
+	if (count == 0) returnStr = '妖精';
+	if (count == 1) returnStr = '皇家';
+	if (count == 2) returnStr = '死靈';
+	if (count == 3) returnStr = '復仇者';
+	if (count == 4) returnStr = '龍族';
+	if (count == 5) returnStr = '吸血鬼';
+	if (count == 6) returnStr = '巫師';
+	if (count == 7) returnStr = '主教';
+
+
+
+
+
+
+	return returnStr;
+	//return rply;
+
+}
+
+
+module.exports = {
+	MultiDrawBan,
+	NomalDrawBan,
+	BanCardReply	
+};
