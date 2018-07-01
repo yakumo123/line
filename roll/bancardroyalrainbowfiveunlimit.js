a51d7d2fd212b44c01ac6e565afd7ec31ff6533a
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-07-01 17:30:48 +0800

    Create bancardroyalrainbowfiveunlimit.js

---
+++ roll/bancardroyalrainbowfiveunlimit.js
@@ -0,0 +1,130 @@
+var rollbase = require('./rollbase.js');
+var rply ={type : 'text'}; //type是必需的,但可以更改
+
+
+function MultiDrawBan(CardToCal, text, type) {
+	let returnStr = '';
+	var tmpcard = 0;
+	var cards = [];
+	var revs = [];
+	var i = 0;
+
+	if (type == 1) 
+	{
+	cards[0] = rollbase.FunnyDice(18); 
+	revs[0] = rollbase.FunnyDice(2);
+
+	for (i = 1; i < 3; i++) {
+		for (;;) {
+		tmpcard = rollbase.FunnyDice(18);
+		if (cards.indexOf(tmpcard) === -1) 
+		{
+			cards.push(tmpcard);
+			revs[i] = rollbase.FunnyDice(2);
+			break;
+		}
+		}
+	}
+
+	if (text != null)
+		returnStr += text + ': \n';
+
+	for (i = 0; i < 3; i++) {
+		if (i == 0) returnStr += '1: ' + BanCardReply(cards[i]) + ' ' + BanRevReply(revs[i]) + '\n';
+		if (i == 1) returnStr += '1: ' + BanCardReply(cards[i]) + ' ' + BanRevReply(revs[i]) + '\n';
+		if (i == 2) returnStr += '1: ' + BanCardReply(cards[i]) + ' ' + BanRevReply(revs[i]);
+	}
+
+	} else if (type == 2) 
+	{
+	cards[0] = rollbase.FunnyDice(18);
+	revs[0] = rollbase.FunnyDice(2);
+
+	for (i = 1; i < 5; i++) {
+		for (;;) {
+		tmpcard = rollbase.FunnyDice(10);
+		if (cards.indexOf(tmpcard) === -1) //沒有重複，就這張了
+		{
+			cards.push(tmpcard);
+			revs[i] = rollbase.FunnyDice(2);
+			break;
+		}
+		}
+	}
+
+	if (text != null)
+		returnStr += text + ': \n';
+
+	for (i = 0; i < 5; i++) {
+		if (i == 0) returnStr += '1: ' + BanCardReply(cards[i]) + ' ' + '\n';
+		if (i == 1) returnStr += '2: ' + BanCardReply(cards[i]) + ' ' + '\n';
+		if (i == 2) returnStr += '3: ' + BanCardReply(cards[i]) + ' ' + '\n';
+    if (i == 3){
+		if (revs[i] == 0) 
+			returnStr += '4: ' + BanCardReply(cards[i]) + ' ' + '\n';
+		else
+			returnStr += '4: ' + BanCardReply(cards[i]) + ' ' + '\n';
+		}
+		if (i == 4) returnStr += '5: ' + BanCardReply(cards[i]) + ' ' ;
+
+	}
+
+	} else {
+
+	if (text == null)
+		returnStr = BanCardReply(rollbase.FunnyDice(18)) + ' ' ;
+	else
+		returnStr = BanCardReply(rollbase.FunnyDice(18)) + ' ' + ' ; ' + text;
+	}
+
+
+	rply.text = returnStr;
+	return rply;
+}
+
+
+function NomalDrawBan(CardToCal, text) {
+	let returnStr = '';
+
+	if (text == null)
+	returnStr = BanCardReply(rollbase.FunnyDice(22)) + ' ' ;
+	else
+	returnStr = BanCardReply(rollbase.FunnyDice(22)) + ' ' + ' ; ' + text;
+	rply.text = returnStr;
+	return rply;
+}
+
+function BanCardReply(count) {
+	let returnStr = '';
+	if (count == 0) returnStr = '天空指揮官‧榭莉亞-虹';
+	if (count == 1) returnStr = '仙杜瑞拉-虹';
+	if (count == 2) returnStr = '沉默的炎將‧瑪爾斯-虹';
+	if (count == 3) returnStr = '斯巴達克斯-虹';
+	if (count == 4) returnStr = '黑暗劍士‧梅麗莎-虹';
+	if (count == 5) returnStr = '巴巴羅薩-虹';
+	if (count == 6) returnStr = '鮮紅的穿光‧瑟塔-虹';
+	if (count == 7) returnStr = '騎士王‧亞瑟-虹';
+	if (count == 8) returnStr = '遠古英雄-虹';
+	if (count == 9) returnStr = '高潔的騎士‧雷瑟姆-虹';
+	if (count == 10) returnStr = '空天城塞-虹';
+if (count == 11) returnStr = '皇家劍士‧奧蕾莉亞-虹';
+if (count == 12) returnStr = '海底都市霸主‧乙姬-虹';
+if (count == 13) returnStr = '椿-虹';
+if (count == 14) returnStr = '圓桌武士‧高文-虹';
+if (count == 15) returnStr = '不滅英傑‧羅蘭-虹';
+if (count == 16) returnStr = '雷維翁劍士‧阿爾貝爾-虹';
+if (count == 17) returnStr = '亞歷山大-虹';
+
+
+
+	return returnStr;
+	//return rply;
+
+}
+
+
+module.exports = {
+	MultiDrawBan,
+	NomalDrawBan,
+	BanCardReply	
+};
