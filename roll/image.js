f47475a9df03efb777a67d152006c299f9bf313f
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-07-05 07:52:43 +0800

    Update image.js

--- roll/image.js
+++ roll/image.js
@@ -3,10 +3,19 @@ var funny = require('./funny.js');
 var rply = { type: 'text' }; //type是必需的,但可以更改
 
 function imsnow() {
-	event.rply = ([
-  { type: 'text', text: 'Hello, world 1' },
-  { type: 'text', text: 'Hello, world 2' }
-]);
+	event.rply = {  
+   "message":"The request body has 2 error(s)",
+   "details":[  
+      {  
+         "message":"May not be empty",
+         "property":"messages[0].text"
+      },
+      {  
+         "message":"Must be one of the following values: [text, image, video, audio, location, sticker, template, imagemap]",
+         "property":"messages[1].type"
+      }
+   ]
+};
 	return.rply	
 }
 
