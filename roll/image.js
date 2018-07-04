3cb5d674fd185b7dcf65bba586215bceebf2138e
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-07-05 00:13:04 +0800

    Update image.js

--- roll/image.js
+++ roll/image.js
@@ -4,10 +4,9 @@ var rply = { type: 'text' }; //type是必需的,但可以更改
 
 function imsnow() {
 	rply = {
-		"type": "image",
-		"originalContentUrl": "https://i.imgur.com/5zrj83B.png",
-		"previewImageUrl": "https://i.imgur.com/5zrj83B.png"
-		}
+    "type": "image",
+    "originalContentUrl": "https://i.imgur.com/5zrj83B.png",
+    "previewImageUrl": "https://i.imgur.com/5zrj83B.png"
 };
 	return rply;
 }
