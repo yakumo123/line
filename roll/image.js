7161a1b57c2f2e7b3e446162e8e40c807217c7c5
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-07-05 08:22:08 +0800

    Update image.js

--- roll/image.js
+++ roll/image.js
@@ -3,20 +3,12 @@ var funny = require('./funny.js');
 var rply = { type: 'text' }; //type是必需的,但可以更改
 
 function imsnow() {
-	rply = {  
-   "message":"The request body has 2 error(s)",
-   "details":[  
-      {  
-         "message":"May not be empty",
-         "property":"messages[0].text"
-      },
-      {  
-         "message":"Must be one of the following values: [text, image, video, audio, location, sticker, template, imagemap]",
-         "property":"messages[1].type"
-      }
-   ]
+	rply = {
+    "type": "image",
+    "originalContentUrl": "https://i.imgur.com/5zrj83B.png",
+    "previewImageUrl": "https://i.imgur.com/5zrj83B.png"
 };
-	return.rply	
+	return rply;
 }
 
 
