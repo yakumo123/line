3ee2e49e4dac724faf640c78a2c6e5be09ec07f7
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-07-05 17:58:42 +0800

    Update image.js

--- roll/image.js
+++ roll/image.js
@@ -1,22 +1,25 @@
 var rollbase = require('./rollbase.js');
 var funny = require('./funny.js');
-var rply = ([{ type: 'text'},{ type: 'text' }]);
+var rply = [{ type: 'text'},{ type: 'text' }];
 
 
 function imsnow() {
-	rply = {
-		[
-  {
-	  type: 'text',
-	  text: 'Hello, world 1'
-  },
-  { 
-	  type: 'text',
-	  text: 'Hello, world 2'
-  }
-]
-	};
+	rply = "events": [
 
+			{
+				"type": "image",
+				"originalContentUrl": "https://i.imgur.com/5zrj83B.png",
+				"previewImageUrl": "https://i.imgur.com/5zrj83B.png"		
+			},
+			{
+				"type": "image",
+				"originalContentUrl": "https://i.imgur.com/5zrj83B.png",
+				"previewImageUrl": "https://i.imgur.com/5zrj83B.png"
+	
+			}			
+		]
+	};
+		
 	return rply; 
 	
 }
