50f59755f2dfe92b6a2ec5d79e2d4b2ca7504c21
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-07-05 08:17:28 +0800

    Update image.js

--- roll/image.js
+++ roll/image.js
@@ -4,17 +4,34 @@ var rply = { type: 'text' }; //type是必需的,但可以更改
 
 function imsnow() {
 	rply = {
-		"events": [
-			{
-				type: 'text',
-				text: 'Hello, world 1' 
-			},
-			{
-				type: 'text',
-				text: 'Hello, world 2' 
-			};
-		]
-	};
+  "events": [
+    {
+      "replyToken": "0f3779fba3b349968c5d07db31eab56f",
+      "type": "message",
+      "timestamp": 1462629479859,
+      "source": {
+        "type": "user",
+        "userId": "U4af4980629..."
+      },
+      "message": {
+        "id": "325708",
+        "type": "text",
+        "text": "Hello, world"
+      }
+    },
+    {
+      "replyToken": "8cf9239d56244f4197887e939187e19e",
+      "type": "follow",
+      "timestamp": 1462629479859,
+      "source": {
+        "type": "user",
+        "userId": "U4af4980629..."
+      }
+    }
+  ]
+}
+
+;
 	return.rply	
 }
 
