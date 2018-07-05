8e30c2d4dc6f0fa7a102a0cf2f01f80a7f44d8be
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-07-05 08:14:14 +0800

    Update image.js

--- roll/image.js
+++ roll/image.js
@@ -3,9 +3,8 @@ var funny = require('./funny.js');
 var rply = { type: 'text' }; //type是必需的,但可以更改
 
 function imsnow() {
-	event.rply = {
+	rply = {
 		"events": [
-
 			{
 				type: 'text',
 				text: 'Hello, world 1' 
