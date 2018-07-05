a34607f8bfc93457029cf4f8968f706961fe0987
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-07-05 18:19:59 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -45,7 +45,9 @@ function parseInput(rplyToken, inputStr) {
 
 	if (trigger.match(/^trpghelp$/)!= null ) return exports.trpghelp.TRPG();
 
-
+	if (trigger.match(/摸八雲/) != null) return exports.funny.randomYakumoKii() ;	
+	
+	
 	if (trigger.match(/(^cc7版創角$|^cc七版創角$)/) != null && mainMsg[1] != NaN )	return exports.coc.build7char(mainMsg[1]);
 	
 	if (trigger.match(/(^cc6版創角$|^cc六版創角$)/) != null && mainMsg[1] != NaN )	return exports.coc.build6char(mainMsg[1]);
