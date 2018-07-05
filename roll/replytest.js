796435899b0b28057f834853f94fbbf8f9dce3a8
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-07-05 13:01:17 +0800

    Create replytest.js

---
+++ roll/replytest.js
@@ -0,0 +1,25 @@
+
+var rollbase = require('./rollbase.js');
+var funny = require('./funny.js');
+var rply = { type: 'text' };
+
+function Remessage() {
+rply = {
+    "replyToken":"nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+    "messages":[
+        {
+            "type":"text",
+            "text":"Hello, user"
+        },
+        {
+            "type":"text",
+            "text":"May I help you?"
+        }
+    ]
+};
+reture rply;
+}
+
+module.exports = {
+Remessage: Remessage
+}
