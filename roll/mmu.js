276accd2e1aeccbe3fd4fbcdb27355cfeb234ce5
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-07-15 17:15:27 +0800

    Create mmu.js

---
+++ roll/mmu.js
@@ -0,0 +1,32 @@
+var rollbase = require('./rollbase.js');
+
+var funny = require('./funny.js');
+
+var rply = { type: 'text' }; //type是必需的,但可以更改
+
+
+function ummm() {
+
+	rply = {
+
+    "type": "image",
+
+    "originalContentUrl": "https://drive.google.com/uc?export=download&id=1lhv1EdOJmMacaw4tTgomhhM-saIB-a7mhttps://drive.google.com/uc?export=download&id=1NXC7gY9CMKp5UbduwhPvFlkry7mYMQq2",
+
+    "previewImageUrl": "https://drive.google.com/uc?export=download&id=1lhv1EdOJmMacaw4tTgomhhM-saIB-a7mhttps://drive.google.com/uc?export=download&id=1NXC7gY9CMKp5UbduwhPvFlkry7mYMQq2"
+
+};
+
+	return rply;
+
+}
+
+
+
+
+module.exports = {
+
+	ummm: ummm
+
+};
+
