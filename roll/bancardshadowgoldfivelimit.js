87c90a04fe0938fff27d3926f10b2ab44f8bc901
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-07-20 10:20:36 +0800

    Update bancardshadowgoldfivelimit.js

--- roll/bancardshadowgoldfivelimit.js
+++ roll/bancardshadowgoldfivelimit.js
@@ -41,40 +41,42 @@ function MultiDrawBan(CardToCal, text, type) {
 	cards[0] = rollbase.FunnyDice(14);
 	revs[0] = rollbase.FunnyDice(2);
 
-	for (i = 1; i < 5; i++) {
-		for (;;) {
-		tmpcard = rollbase.FunnyDice(14);
-		if (cards.indexOf(tmpcard) === -1) //沒有重複，就這張了
-		{
-			cards.push(tmpcard);
-			revs[i] = rollbase.FunnyDice(2);
-			break;
+		for (i = 1; i < 5; i++) {
+			for (;;) {
+			tmpcard = rollbase.FunnyDice(10);
+			if (cards.indexOf(tmpcard) === -1) //沒有重複，就這張了
+			{
+				cards.push(tmpcard);
+				revs[i] = rollbase.FunnyDice(2);
+				break;
+			}
+			}
 		}
+	
+
+		if (text != null)
+			returnStr += text + ': \n';
+	
+
+		for (i = 0; i < 5; i++) {
+			if (i == 0) returnStr += '1: ' + BanCardReply(cards[i]) + ' ' + '\n';
+			if (i == 1) {
+			if (revs[i] == 0)
+				returnStr += '2: ' + BanCardReply(cards[i]) + ' ' + '\n';
+			else
+				returnStr += '2: ' + BanCardReply(cards[i]) + ' ' + '\n';
+			}
+			if (i == 2) returnStr += '3: ' + BanCardReply(cards[i]) + ' ' + '\n';
+			if (i == 3) {
+			if (revs[i] == 0) 
+				returnStr += '4: ' + BanCardReply(cards[i]) + ' ' + '\n';
+			else
+				returnStr += '4: ' + BanCardReply(cards[i]) + ' ' + '\n';
+			}
+			if (i == 4) returnStr += '5: ' + BanCardReply(cards[i]) + ' ' ;
+	
+
 		}
-	}
-
-	if (text != null)
-		returnStr += text + ': \n';
-
-	for (i = 0; i < 5; i++) {
-		if (i == 0) returnStr += '1: ' + BanCardReply(cards[i]) + ' ' + '\n';
-		if (i == 1) {
-		if (revs[i] == 0)
-			returnStr += '2: ' + BanCardReply(cards[i]) + ' ' + '\n';
-		else
-			returnStr += '2: ' + BanCardReply(cards[i]) + ' ' + '\n';
-		}
-		if (i == 2) returnStr += '3: ' + BanCardReply(cards[i]) + ' ' + '\n';
-		if (i == 3) {
-		if (revs[i] == 0) 
-			returnStr += '4: ' + BanCardReply(cards[i]) + ' ' + '\n';
-		else
-			returnStr += '4: ' + BanCardReply(cards[i]) + ' ' + '\n';
-		}
-		if (i == 4) returnStr += '5: ' + BanCardReply(cards[i]) + ' ' ;
-
-	}
-
 	} else {
 
 	if (text == null)
