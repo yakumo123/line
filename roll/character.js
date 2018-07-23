838b80b32f3e15ba5f42daa8c3ee1d2fa8dda092
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-07-23 14:10:38 +0800

    Update character.js

--- roll/character.js
+++ roll/character.js
@@ -17,14 +17,21 @@ function build7char(text01){
 	old = text01;
 	ReStr = ReStr + old + '\n';
 	}
+	//設定 因年齡減少的點數 和 EDU加骰次數
 	let Debuff = 0;
-	let levelArr = [0,10,20,30,40,50,60,70,80,90,100]
+	let AppDebuff = 0;
+	let EDUinc = 0;
+	let levelArr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,11,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,82,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100]
 	let level = levelArr[Math.floor((Math.random() * (levelArr.length)) + 0)];
 	let oldArr = [15,20,40,50,60,70,80]
-	let DebuffaRR = [0,1,2,3,4,4,4]
+	let DebuffArr = [5,0,5,10,20,40,80]
+	let AppDebuffArr = [0,0,5,10,15,20,25]
+	let EDUincArr = [0,1,2,3,4,4,4]
 	
 	for ( i=0 ; level >= levelArr[i] ; i ++){
 		Debuff = DebuffArr[i];
+		AppDebuff = AppDebuffArr[i];
+		EDUinc = EDUincArr[i];
 	}
 	ReStr = ReStr + '==\n';
 	if (level < 10) ReStr = ReStr + 'test1' ;
@@ -46,22 +53,22 @@ function build7char(text01){
 		if (level >= 80 && level <90)	ReStr = ReStr + 'test9' ;
 	else
 		if (level >= 90 && level <100)	ReStr = ReStr + 'test4' ;
-	else ReStr = ReStr + 'test11' ;
+	else ReStr = ReStr + '年齡調整：可做' + EDUinc + '次EDU的成長擲骰。' ;
 	ReStr = ReStr + '\n==';
-	if (level>=40) ReStr = ReStr ;
-	if (level<20) ReStr = ReStr ;
+	if (level>=40) ReStr = ReStr + '\n（以下箭號三項，自選共減' + Debuff + '點。）' ;
+	if (level<20) ReStr = ReStr + '\n（以下箭號兩項，擇一減去' + Debuff + '點。）' ;
 	ReStr = ReStr + '\nＳＴＲ：' + rollbase.BuildDiceCal('3d6*5') + level;
-	if (level>=40) ReStr = ReStr ;
-	if (level<20) ReStr = ReStr ;
+	if (level>=40) ReStr = ReStr + ' ← 共減' + Debuff ;
+	if (level<20) ReStr = ReStr + ' ←擇一減' + Debuff ;
 	ReStr = ReStr + '\nＣＯＮ：' + rollbase.BuildDiceCal('3d6*5') + level;
-	if (level>=40) ReStr = ReStr ;
+	if (level>=40) ReStr = ReStr + ' ← 共減' + Debuff;
 	ReStr = ReStr + '\nＤＥＸ：' + rollbase.BuildDiceCal('3d6*5');
-	if (level>=40) ReStr = ReStr ;
+	if (level>=40) ReStr = ReStr + ' ← 共減' + Debuff ;
 	if (level>=40) ReStr = ReStr + '\nＡＰＰ：' + rollbase.BuildDiceCal('3d6*5-' + AppDebuff);
 	else ReStr = ReStr + '\nＡＰＰ：' + rollbase.BuildDiceCal('3d6*5');
 	ReStr = ReStr + '\nＰＯＷ：' + rollbase.BuildDiceCal('3d6*5');
 	ReStr = ReStr + '\nＳＩＺ：' + rollbase.BuildDiceCal('(2d6+6)*5');
-	if (level<20) ReStr = ReStr ;
+	if (level<20) ReStr = ReStr + ' ←擇一減' + Debuff ;
 	ReStr = ReStr + '\nＩＮＴ：' + rollbase.BuildDiceCal('(2d6+6)*5');	
 	if (level<20) ReStr = ReStr + '\nＥＤＵ：' + rollbase.BuildDiceCal('3d6*5-5');
 	else {
