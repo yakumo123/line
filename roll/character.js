6dbbab4140f499d7bf3c8b61ecba845c219ccf87
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-07-23 13:43:27 +0800

    Update character.js

--- roll/character.js
+++ roll/character.js
@@ -87,10 +87,10 @@ function build7char(text01){
 	ReStr = ReStr + '\n==';
 	if (level>=40) ReStr = ReStr + '\n（以下箭號三項，自選共減' + Debuff + '點。）' ;
 	if (level<20) ReStr = ReStr + '\n（以下箭號兩項，擇一減去' + Debuff + '點。）' ;
-	ReStr = ReStr + '\nＳＴＲ：' + rollbase.BuildDiceCal('3d6*5') + level;
+	ReStr = ReStr + '\nＳＴＲ：' + rollbase.BuildDiceCal('3d6*5');
 	if (level>=40) ReStr = ReStr + ' ← 共減' + Debuff ;
 	if (level<20) ReStr = ReStr + ' ←擇一減' + Debuff ;
-	ReStr = ReStr + '\nＣＯＮ：' + rollbase.BuildDiceCal('3d6*5') + level;
+	ReStr = ReStr + '\nＣＯＮ：' + rollbase.BuildDiceCal('3d6*5');
 	if (level>=40) ReStr = ReStr + ' ← 共減' + Debuff;
 	ReStr = ReStr + '\nＤＥＸ：' + rollbase.BuildDiceCal('3d6*5');
 	if (level>=40) ReStr = ReStr + ' ← 共減' + Debuff ;
