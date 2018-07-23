697834fc1efde287a04935cdd9bfc7bfbffeeeee
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-07-23 13:50:36 +0800

    Update character.js

--- roll/character.js
+++ roll/character.js
@@ -1,6 +1,6 @@
 var rollbase = require('./rollbase.js');
 var rply ={type : 'text'}; //type是必需的,但可以更改
-let levelArr = [0','10','20','30','40','50','60','70','80','90','100'];
+let levelArr = [0','10','20','30','40','50','60','70','80','90','100'
 let level = levelArr[Math.floor((Math.random() * (levelArr.length)) + 0)];
 
 ////////////////////////////////////////
