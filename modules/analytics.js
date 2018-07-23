06390015ff4dba1c6749dd981ce97f190c8f3d17
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-07-23 18:00:33 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -51,6 +51,8 @@ function parseInput(rplyToken, inputStr) {
 	
 	if (trigger.match(/(能力值$)/) != null && mainMsg[1] != NaN )	return exports.character.build7char(mainMsg[1]);
 	
+	if (trigger.match(/test/) != null) return exports.character.ability(mainMsg) ;	
+
 	
 	if (trigger.match(/(^cc7版創角$|^cc七版創角$)/) != null && mainMsg[1] != NaN )	return exports.coc.build7char(mainMsg[1]);
 	
