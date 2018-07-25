c24ad14d302a44188438371b73e4ba29d0950588
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-07-25 23:33:31 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -54,7 +54,7 @@ function parseInput(rplyToken, inputStr) {
 	
 	if (trigger.match(/綜合分數$/) != null) return exports.character.ability(mainMsg) ;	
 
-	if (trigger.match(/(生成卡片$)/) != null && mainMsg[1] != NaN )	return exports.createcard.build7char(mainMsg[1]);
+	if (trigger.match(/^生成卡片$|^卡片生成$|^生成卡牌$|^卡牌生成$/) != null && mainMsg[1] != NaN )	return exports.createcard.build7char(mainMsg[1]);
 	
 	
 	if (trigger.match(/(^cc7版創角$|^cc七版創角$)/) != null && mainMsg[1] != NaN )	return exports.coc.build7char(mainMsg[1]);
