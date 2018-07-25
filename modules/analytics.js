3094df567a830f159585b379d3d79fc057dfd491
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-07-25 17:48:50 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -49,9 +49,11 @@ function parseInput(rplyToken, inputStr) {
 
 	if (trigger.match(/摸八雲|摸爆八雲/) != null) return exports.funny.randomYakumoKii() ;	
 	
-	if (trigger.match(/(能力值$)/) != null && mainMsg[1] != NaN )	return exports.character.build7char(mainMsg[1]);
+	if (trigger.match(/能力值$)/) != null && mainMsg[1] != NaN )	return exports.character.build7char(mainMsg[1]);
 	
 	if (trigger.match(/綜合分數$/) != null) return exports.character.ability(mainMsg) ;	
+
+	if (trigger.match(/^test$)/) != null && mainMsg[1] != NaN )	return exports.createcard.build7char(mainMsg[1]);
 
 	
 	if (trigger.match(/(^cc7版創角$|^cc七版創角$)/) != null && mainMsg[1] != NaN )	return exports.coc.build7char(mainMsg[1]);
