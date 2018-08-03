9d2a7a363cf4fca57f7e44b80ae70801494cb0fb
yakumoouo <40012604+yakumoouo@users.noreply.github.com>
2018-08-03 21:35:50 +0800

    Update button.js

--- roll/button.js
+++ roll/button.js
@@ -4,28 +4,49 @@ var rply = { type: 'text' }; //type是必需的,但可以更改
 
 function buttontest() {
 	rply = {
-    "type": "template",
-    "altText": "SV活動情報",
-    "template": {
+  "type": "template",
+  "altText": "this is a carousel template",
+  "template": {
+      "type": "carousel",
+      "columns": [			
+	      {
+		      "type": "buttons",
+		      "thumbnailImageUrl": "https://shadowverse.jp/news/wp-content/uploads/e0b558ba389c34594d109581059d77f1.png",
+		      "title": "SV活動情報",        
+		      "text": "活動情報",
+		      "actions": [
+			      {
+				      "type": "uri",
+				      "label": "Event Support情報",
+				      "uri": "https://event.shadowverse.com/cht/eventsupport/"
+			      },
+			      {
+				      "type": "uri",
+				      "label": "ShadowNews",
+				      "uri": "https://shadowverse.com/sp/cht/news/"
+			      },	
+			      {
+				      "type": "uri",				      
+				      "label": "勝率分析",
+				      "uri": "https://shadowlog.com/s/trend/"
+			      }
+		      ]
+	      }
+	      {
         "type": "buttons",
         "thumbnailImageUrl": "https://i.ytimg.com/vi/G8O2TSRilkg/maxresdefault.jpg",
-        "title": "SV活動情報",
-        "text": "活動情報",
+        "title": "牌組參考",
+        "text": "牌組參考",
         "actions": [
             {
                 "type": "uri",
-                "label": "Event Support情報",
-                "uri": "https://event.shadowverse.com/cht/eventsupport/"
+                "label": "指定牌組參考",
+                "uri": "https://altema.jp/shadowverse/saikyodeck"
             },
             {
                 "type": "uri",
-                "label": "ShadowNews",
-                "uri": "https://shadowverse.com/sp/cht/news/"
-            },
-            {
-                "type": "uri",
-                "label": "勝率分析",
-                "uri": "https://shadowlog.com/s/trend/"
+                "label": "無限制牌組參考",
+                "uri": "https://altema.jp/shadowverse/anrimisaikyou"
             }
         ]
     }
