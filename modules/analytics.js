580c2e8d057bba3f1f50633732db21df1d0d8bf0
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-08-12 13:50:19 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -79,6 +79,7 @@ function parseInput(rplyToken, inputStr) {
 	if (trigger.match(/^問號$/)!= null ) return exports.kiddingme.joking();
 	
 	if (trigger.match(/^sv情報$/)!= null ) return exports.button.buttontest();
+	if (trigger.match(/^公主情報$/)!= null ) return exports.redive.princesses();
 	
 	if (trigger.match(/^八雲比賽牌組$/)!= null ) return exports.yakumocard.buttontest();
 	
