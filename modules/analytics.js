0a5a1db1386e8800890b534c184298e001525700
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-08-18 08:50:11 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -351,9 +351,7 @@ function parseInput(rplyToken, inputStr) {
 	if (trigger.match(/^兔子$|燻兔肉佐/) != null) return exports.funny.randomRabbit() ;	
 	if (trigger.match(/燻兔肉佐/) != null) return exports.funny.randomRabbit() ;	
 	if (trigger.match(/^@高睿鴻$|^笑臉$/) != null) return exports.funny.randomSmileFace() ;
-	
-	if (trigger.match(/叔叔/) != null) return exports.funny.randomShit() ;	
-	
+		
 	if (trigger.match(/舔吹雪/) != null) return exports.image.imsnow() ;	
 	
 	if (trigger.match(/摸吹雪/) != null) return exports.imagetwo.immsnow() ;	
