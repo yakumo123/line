df1edc061fd45218d5147bf351412899ec2c8974
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-08-19 15:18:17 +0800

    Create randomphoto.js

---
+++ roll/randomphoto.js
@@ -0,0 +1,34 @@
+var rollbase = require('./rollbase.js');
+
+var funny = require('./funny.js');
+
+var rply = { type: 'text' }; //type是必需的,但可以更改
+
+let ArrLinkID = ['J8GLWvT.jpg','ccC24kA.jpg','7tsp2jz.jpg']
+let Link = 'https://i.imgur.com/' + LinkID
+
+
+function RPhoto() {
+
+	rply = {
+
+    "type": "image",
+
+    "originalContentUrl": "https://i.imgur.com/p7c2LbE.png",
+
+    "previewImageUrl": "https://i.imgur.com/p7c2LbE.png"
+
+};
+
+	return rply;
+
+}
+
+
+
+
+module.exports = {
+
+	RPhoto: RPhoto
+
+};
