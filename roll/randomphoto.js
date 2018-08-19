b15e9a461dba2bf8cfdbf34a3529bd1dc182950d
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-08-19 16:10:38 +0800

    Update randomphoto.js

--- roll/randomphoto.js
+++ roll/randomphoto.js
@@ -4,8 +4,8 @@ var funny = require('./funny.js');
 
 var rply = { type: 'text' }; //type是必需的,但可以更改
 
-let LinkIDArr = ['"https://i.imgur.com/J8GLWvT.jpg"','"https://i.imgur.com/ccC24kA.jpg"','"https://i.imgur.com/7tsp2jz.jpg"']
-let LinkID = LinkIDArr[Math.floor((Math.random() * (LinkIDArrlevelArr.length)) + 0)];
+let LinkIDArr = ['https://i.imgur.com/J8GLWvT.jpg','https://i.imgur.com/ccC24kA.jpg','https://i.imgur.com/7tsp2jz.jpg']
+let LinkID = LinkIDArr[Math.floor((Math.random() * (LinkIDArr.length)) + 0)];
 
 
 function RPhoto() {
