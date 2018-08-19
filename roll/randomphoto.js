8e034aec972374679ecba7548faf680200f3bf98
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-08-19 16:16:41 +0800

    Update randomphoto.js

--- roll/randomphoto.js
+++ roll/randomphoto.js
@@ -6,8 +6,8 @@ var rply = { type: 'text' }; //type是必需的,但可以更改
 
 function RPhoto() {
 	
-let LinkIDArr = ['"https://i.imgur.com/J8GLWvT.jpg"','"https://i.imgur.com/ccC24kA.jpg"','"https://i.imgur.com/7tsp2jz.jpg"']
-let LinkID = LinkIDArr[Math.floor((Math.random() * (LinkIDArr.length)) + 0)];
+var LinkIDArr = ['https://i.imgur.com/J8GLWvT.jpg','https://i.imgur.com/ccC24kA.jpg','https://i.imgur.com/7tsp2jz.jpg']
+var LinkID = LinkIDArr[Math.floor((Math.random() * (LinkIDArr.length)) + 0)];
 
 var LinkID = Link
 	rply = {
