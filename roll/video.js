ca49189aa2dbc02891ea8522c8d75378ef3733c7
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-08-25 00:12:54 +0800

    Update video.js

--- roll/video.js
+++ roll/video.js
@@ -5,8 +5,8 @@ var rply = { type: 'text' }; //type是必需的,但可以更改
 function mushroomcloud() {
 	rply = {
     "type": "video",
-    "originalContentUrl": "https://drive.google.com/uc?export=download&id=1h7GhClMfoCxXCXdjVV8wiFylaMz-_IIk",
-    "previewImageUrl": "https://pic.dmyy.cc/Uploads/vod/2014-08-17/sxdd.jpg"
+    "originalContentUrl": "https://drive.google.com/uc?export=download&id=1B9Y2W_vsG_bh3byblJcqx4t5sSIoS8Y5",
+    "previewImageUrl": "https://blogimg.goo.ne.jp/user_image/3c/df/069e3d399fefba1ab270f69ddcb204a6.jpg"
 };
 	return rply;
 }
