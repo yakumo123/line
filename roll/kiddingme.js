1fd71f43f4f8bd6805843e5ce2c322e8b4b7e457
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-08-24 11:44:27 +0800

    Update kiddingme.js

--- roll/kiddingme.js
+++ roll/kiddingme.js
@@ -11,44 +11,13 @@ function joking() {
       "columns": [
           {
             "imageUrl": "https://i.imgur.com/wODPWY8.jpg",
-            "action": {
-              "type": "message",
-              "label": "黑人問號",
-              "text": "！！！∑(°Д°ノ)ノ"
-            }
           },
           {
             "imageUrl": "https://i.imgur.com/VIpVQRd.png",
-            "action": {
-              "type": "message",
-              "label": "八雲問號",
-              "text": "Σ( ° △ °|||)︴"
-            }
           },
           {
             "imageUrl": "https://i.imgur.com/EJf8CZ3.jpg",
-            "action": {
-              "type": "message",
-              "label": "白人問號",
-              "text": "我智障到醫生說無可救藥…"
-	    }
-	  },
-          {
-            "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/d/dd/Achtung.svg",
-            "action": {
-              "type": "message",
-              "label": "感嘆號",
-              "text": "我智障到醫生說無可救藥…"
-	    }
-	  },
-	      {
-            "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/5/59/Triangle_warning_sign_%28red_and_white%29.svg",
-            "action": {
-              "type": "message",
-              "label": "感嘆號沒感嘆",
-              "text": "我智障到醫生說無可救藥…"
-            }
-          }
+	  }
       ]
   }
 };
