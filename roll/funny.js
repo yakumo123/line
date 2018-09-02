cd2f8af1a5e6f46e1ee1085c16bc629eab5617bd
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-09-02 19:10:21 +0800

    Update funny.js

--- roll/funny.js
+++ roll/funny.js
@@ -950,7 +950,7 @@ function Poker14() {
 	arr[randomIndex] = arr[i];
 	arr[i] = itemAtIndex;
 	}
-rply.text = rply.text = ' → ' + arr[0] + ' ， ' + arr[1] + ' ， ' + arr[2] + ' ， ' + arr[3] + ' ， ' + arr[4] + ' ， ' + arr[5] + ' ， ' + arr[6] + ' ， ' + arr[7] + ' ， ' + arr[8] + ' ， ' + arr[9] + ' ， ' + arr[10] + ' ， ' + arr[12] + arr[13] ;
+rply.text = rply.text = ' → ' + arr[0] + ' ， ' + arr[1] + ' ， ' + arr[2] + ' ， ' + arr[3] + ' ， ' + arr[4] + ' ， ' + arr[5] + ' ， ' + arr[6] + ' ， ' + arr[7] + ' ， ' + arr[8] + ' ， ' + arr[9] + ' ， ' + arr[10] + ' ， ' + arr[12] + ' ， ' + arr[13] ;
 return rply;
 }
 
@@ -1151,6 +1151,21 @@ rply.text = ' → ' + arr[0] ;
 return rply;
 }
 
+function PokerAll() {	
+
+	let arr = ['♤A','♤2','♤3','♤4','♤5','♤6','♤7','♤8','♤9','♤10','♤J','♤Q','♤K','♡A','♡2','♡3','♡4','♡5','♡6','♡7','♡8','♡9','♡10','♡J','♡Q','♡K','◇A','◇2','◇3','◇4','◇5','◇6','◇7','◇8','◇9','◇10','◇J','◇Q','◇K','◇A','◇2','◇3','◇4','◇5','◇6','◇7','◇8','◇9','◇10','◇J','◇Q','◇K'];
+
+	for (var i = arr.length-1; i >=0; i--) {
+
+	var randomIndex = Math.floor(Math.random()*(i+1));
+	var itemAtIndex = arr[randomIndex];
+	arr[randomIndex] = arr[i];
+	arr[i] = itemAtIndex;
+	}
+rply.text = rply.text = 'Player 1：' + arr[0] + ' ， ' + arr[1] + ' ， ' + arr[2] + ' ， ' + arr[3] + ' ， ' + arr[4] + ' ， ' + arr[5] + ' ， ' + arr[6] + ' ， ' + arr[7] + ' ， ' + arr[8] + ' ， ' + arr[9] + ' ， ' + arr[10] + ' ， ' + arr[12] + ' ， ' + arr[13] + '\n\nPlayer 2：' + arr[14] + ' ， ' + arr[15] + ' ， ' + arr[16] + ' ， ' + arr[17] + ' ， ' + arr[18] + ' ， ' + arr[19] + ' ， ' + arr[20] + ' ， ' + arr[21] + ' ， ' + arr[22] + ' ， ' + arr[23] + ' ， ' + arr[24] + ' ， ' + arr[25] + ' ， ' + arr[26] + '\n\nPlayer 3：' + arr[27] + ' ， ' + arr[28] + ' ， ' + arr[29] + ' ， ' + arr[30] + ' ， ' + arr[31] + ' ， ' + arr[32] + ' ， ' + arr[33] + ' ， ' + arr[34] + ' ， ' + arr[35] + ' ， ' + arr[36] + ' ， ' + arr[37] + ' ， ' + arr[38] + ' ， ' + arr[39] + '\n\nPlayer 2：' + arr[40] + ' ， ' + arr[41] + ' ， ' + arr[42] + ' ， ' + arr[43] + ' ， ' + arr[44] + ' ， ' + arr[45] + ' ， ' + arr[46] + ' ， ' + arr[47] + ' ， ' + arr[48] + ' ， ' + arr[49] + ' ， ' + arr[50] + ' ， ' + arr[51] + ' ， ' + arr[52] ;
+return rply;
+}
+
 module.exports = {
 	know,
 	lullaby,
@@ -1210,5 +1225,6 @@ module.exports = {
 	Poker4,
 	Poker3,
 	Poker2,
-	Poker1
+	Poker1,
+	PokerAll
 };
