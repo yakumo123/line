565e0a88ae4f3d25b0b9eda7c9be4e5a76d64919
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-09-03 21:43:13 +0800

    Update kidding.js

--- roll/kidding.js
+++ roll/kidding.js
@@ -5,38 +5,46 @@ var rply = { type: 'text' }; //type是必需的,但可以更改
 function joke() {
 	rply = {
   "type": "text",
-  "text": "function test",
+  "text": "選一張撲克牌",
 		"quickReply": {
   "items": [
     {
       "type": "action",
       "action": {
-        "type": "cameraRoll",
-        "label": "Send photo"
-      }
+       "type": "message",
+        "label": "♡A",
+	      "text": "我是智障"  
     },
 	  {
       "type": "action",
       "action": {
-        "type": "message",
-        "label": "Say Hi",
-	      "text": "Hi"
+        "type": "♧J",
+        "label": "",
+	      "text": "我是變態"
       }
     },
-	{
+	 {
       "type": "action",
       "action": {
-        "type": "postback",
-        "label": "Postback Data",
-	      "data": "Hi",
-	      "text": "Hi"
+        "type": "message",
+        "label": "♤2",
+	      "text": "我是蘿莉控"
       }
-    },  
+    },
     {
       "type": "action",
       "action": {
-        "type": "camera",
-        "label": "Open camera"
+        "type": "message",
+        "label": "◇K",
+	      "text": "我是紅茶"
+      }
+    },
+	   {
+      "type": "action",
+      "action": {
+        "type": "message",
+        "label": "♡Q",
+	      "text": "我想被踩"
       }
     }
 ]
