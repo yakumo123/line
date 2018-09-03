aa90622cc5c78a6716f5620101dac309ad990048
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-09-03 09:36:19 +0800

    Update kidding.js

--- roll/kidding.js
+++ roll/kidding.js
@@ -4,22 +4,29 @@ var rply = { type: 'text' }; //type是必需的,但可以更改
 
 function joke() {
 	rply = {
-  "type": "bubble", // ①
-  "body": { // ②
-    "type": "box", // ③
-    "layout": "horizontal",　// ④
-    "contents": [ // ⑤
-      {
-        "type": "text", // ⑥
-        "text": "Hello,"
-      },
-      {
-        "type": "text", // ⑥
-        "text": "World!"
-      }
-    ]
-  }
-};
+  "type": "template",
+  "altText": "test",
+  "template": {
+    "type": "buttons",
+    "text": "test",
+    "actions": [
+          {
+				      "type": "postback",
+				      "label": "test1",
+				      "data": "postback_test"
+			      },
+			      {
+				      "type": "uri",
+				      "label": "ShadowNews",
+				      "uri": "https://shadowverse.com/sp/cht/news/"
+			      },	
+			      {
+				      "type": "uri",				      
+				      "label": "勝率分析",
+				      "uri": "https://shadowlog.com/s/trend/"
+			      }
+        ]
+      },;
 	return rply;
 }
 
