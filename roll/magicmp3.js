26ead0e182bead44148d61bc2958ea0d29fa0bd1
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-09-03 23:53:56 +0800

    Update magicmp3.js

--- roll/magicmp3.js
+++ roll/magicmp3.js
@@ -7,7 +7,7 @@ function magic() {
 	rply = {
 				"type": "audio",
 				"originalContentUrl": "https://drive.google.com/uc?export=download&id=16tYAkJhYrbTADKmdTgQq_mLzNoKdE8ff",
-				"duration": "60000"		
+				"duration": "20000"		
 	};
 		
 	return rply; 
