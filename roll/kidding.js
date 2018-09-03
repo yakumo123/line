f475237bf9f43c1dcf444b90e6b270bf50510aa7
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-09-03 09:45:00 +0800

    Update kidding.js

--- roll/kidding.js
+++ roll/kidding.js
@@ -4,29 +4,25 @@ var rply = { type: 'text' }; //type是必需的,但可以更改
 
 function joke() {
 	rply = {
-  "type": "template",
-  "altText": "test",
-  "template": {
-    "type": "buttons",
-    "text": "test",
-    "actions": [
-          {
-				      "type": "postback",
-				      "label": "test1",
-				      "data": "postback_test"
-			      },
-			      {
-				      "type": "uri",
-				      "label": "ShadowNews",
-				      "uri": "https://shadowverse.com/sp/cht/news/"
-			      },	
-			      {
-				      "type": "uri",				      
-				      "label": "勝率分析",
-				      "uri": "https://shadowlog.com/s/trend/"
-			      }
-        ]
-      };
+		"quickReply": {
+  "items": [
+    {
+      "type": "action",
+      "action": {
+        "type": "cameraRoll",
+        "label": "Send photo"
+      }
+    },
+    {
+      "type": "action",
+      "action": {
+        "type": "camera",
+        "label": "Open camera"
+      }
+    }
+]
+		}
+	};
 	return rply;
 }
 
