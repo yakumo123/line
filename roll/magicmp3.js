d80ec9a8372d1d40a6a4c13221b6c2929a813b99
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-09-03 23:49:04 +0800

    Create magicmp3.js

---
+++ roll/magicmp3.js
@@ -0,0 +1,21 @@
+var rollbase = require('./rollbase.js');
+var funny = require('./funny.js');
+var rply = { type: 'text'};
+
+
+function magic() {
+	rply = {
+				"type": "audio",
+				"originalContentUrl": "https://drive.google.com/uc?export=download&id=16tYAkJhYrbTADKmdTgQq_mLzNoKdE8ff",
+				"duration": "60000"		
+	};
+		
+	return rply; 
+	
+}
+
+
+
+module.exports = {
+	magic: magic
+};
