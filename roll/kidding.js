94c8d07635ddb29f7f3174f3aac530114cd69888
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-09-05 23:17:13 +0800

    Update kidding.js

--- roll/kidding.js
+++ roll/kidding.js
@@ -4,45 +4,45 @@ var rply = { type: 'text' }; //type是必需的,但可以更改
 
 function joke() {
 	rply = {
-		"type": "text",
-		"text": "選一張撲克牌",
-		"quickReply": { 
-			"items": [
-				{
-					"type": "action",
-					"action": {
-						"type": "message",
-						"label": "◇K",
-						"text": "我是紅茶"
-					}
-				},
-				{	
-					"type": "action",
-					"action": {
-						"type": "message",
-						"label": "♧3",
-						"text": "我是變態"
-					}
-				},
-				{
-					"type": "action",
-					"action": {
-						"type": "message",
-						"label": "♡Q",
-						"text": "我想被踩"
-					}
-				},
-				{
-					"type": "action",
-					"action": {
-						"type": "message",
-						"label": "♡A",
-						"text": "我是智障"  
-					}
-				}
-			]
-		}
-	};
+  "type": "bubble",
+  "header": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Header text"
+      }
+    ]
+  },
+  "hero": {
+    "type": "image",
+    "url": "https://drive.google.com/uc?export=download&id=1KNgmkrvog0OnUHRil9seI8CfyVAWtn3j",
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Body text",
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Footer text",
+      }
+    ]
+  },
+  "styles": {
+    "comment": "See the example of a bubble style object"
+  }
+};
 	return rply;
 }
 
