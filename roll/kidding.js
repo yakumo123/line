be319fef68fabc5ae719a2ac76918119011e6216
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-09-05 23:40:55 +0800

    Update kidding.js

--- roll/kidding.js
+++ roll/kidding.js
@@ -7,45 +7,31 @@ function joke() {
   "type": "carousel",
   "contents": [
     {
-  "type": "bubble",
-  "header": {
-    "type": "box",
-    "layout": "vertical",
-    "contents": [
-      {
-        "type": "text",
-        "text": "Header text"
+      "type": "bubble",
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": "First bubble"
+          }
+        ]
       }
-    ]
-  },
-  "hero": {
-    "type": "image",
-    "url": "https://drive.google.com/uc?export=download&id=1KNgmkrvog0OnUHRil9seI8CfyVAWtn3j",
-  },
-  "body": {
-    "type": "box",
-    "layout": "vertical",
-    "contents": [
-      {
-        "type": "text",
-        "text": "Body text",
+    },
+    {
+      "type": "bubble",
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": "Second bubble"
+          }
+        ]
       }
-    ]
-  },
-  "footer": {
-    "type": "box",
-    "layout": "vertical",
-    "contents": [
-      {
-        "type": "text",
-        "text": "Footer text",
-      }
-    ]
-  },
-  "styles": {
-    "comment": "See the example of a bubble style object"
-  }
-}
+    }
   ]
 };
 	return rply;
