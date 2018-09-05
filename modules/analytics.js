cdc0975eaecbea23cce2ed825acc17b6408f2465
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-09-06 00:50:13 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -78,7 +78,7 @@ function parseInput(rplyToken, inputStr) {
 	
 	if (trigger.match(/^comfirmcommand$/)!= null ) return exports.commandlist.Cili();
 
-	if (trigger.match(/(^咕嚕靈波)/)!= null ) return exports.kidding.joke();
+	if (trigger.match(/^咕嚕靈波（●´∀｀）ノ♡$/)!= null ) return exports.kidding.joke();
 	
 	if (trigger.match(/^問號$/)!= null ) return exports.kiddingme.joking();
 	
