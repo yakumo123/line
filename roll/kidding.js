da1ec7b0d23557472e877847880989cbc3fa45f9
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-09-05 23:27:03 +0800

    Update kidding.js

--- roll/kidding.js
+++ roll/kidding.js
@@ -1,6 +1,6 @@
 var rollbase = require('./rollbase.js');
 var funny = require('./funny.js');
-var rply = { type: 'text' }; //type是必需的,但可以更改
+var rply = { type: 'message' }; //type是必需的,但可以更改
 
 function joke() {
 	rply = {  
