cba72164efd0963e6a03fa85e7d5a5612cc067de
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-09-05 23:47:58 +0800

    Update kidding.js

--- roll/kidding.js
+++ roll/kidding.js
@@ -1,21 +1,48 @@
 var rollbase = require('./rollbase.js');
 var funny = require('./funny.js');
-var rply = { type: 'message' }; //type是必需的,但可以更改
+var rply = { type: 'messages' }; //type是必需的,但可以更改
 
 function joke() {
 	rply = {
-      "type": "bubble",
-      "body": {
-        "type": "box",
-        "layout": "vertical",
-        "contents": [
-          {
-            "type": "text",
-            "text": "First bubble"
-          }
-        ]
+  "type": "bubble",
+  "header": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Header text"
       }
-    };
+    ]
+  },
+  "hero": {
+    "type": "image",
+    "url": "https://example.com/flex/images/image.jpg",
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Body text",
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Footer text",
+      }
+    ]
+  },
+  "styles": {
+    "comment": "See the example of a bubble style object"
+  }
+};
 	return rply;
 }
 
