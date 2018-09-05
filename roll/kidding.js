c3fe418185cb609c58a66a77881e46dcf3f8b442
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-09-06 00:13:15 +0800

    Update kidding.js

--- roll/kidding.js
+++ roll/kidding.js
@@ -1,101 +1,44 @@
 var rollbase = require('./rollbase.js');
 var funny = require('./funny.js');
 var message = {
-            "type": "flex",
-            "altText": "#",
-            "contents": {
-              "type": "bubble",
-              "hero": {
-                "type": "image",
-                "url": "https://s3-ap-northeast-1.amazonaws.com/xxxxxxx/hamburger.jpg",
-                "size": "full",
-                "aspectRatio": "20:13",
-                "aspectMode": "cover",
-                "action": {
-                  "type": "uri",
-                  "uri": "https://classmethod.jp/"
-                }
-              },
-              "body": {
-                "type": "box",
-                "layout": "vertical",
-                "spacing": "md",
-                "action": {
-                  "type": "uri",
-                  "uri": "https://classmethod.jp/"
-                },
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "Hamburger",
-                    "size": "xl",
-                    "weight": "bold"
-                  },
-                  {
-                    "type": "box",
-                    "layout": "vertical",
-                    "spacing": "sm",
-                    "contents": [
-                      {
-                        "type": "box",
-                        "layout": "baseline",
-                        "contents": [
-                          {
-                            "type": "icon",
-                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/restaurant_regular_32.png"
-                          },
-                          {
-                            "type": "text",
-                            "text": "$10.5",
-                            "weight": "bold",
-                            "margin": "sm",
-                            "flex": 0
-                          },
-                          {
-                            "type": "text",
-                            "text": "400kcl",
-                            "size": "sm",
-                            "align": "end",
-                            "color": "#aaaaaa"
-                          }
-                        ]
-                      }
-                    ]
-                  },
-                  {
-                    "type": "text",
-                    "text": "Sauce, Onions, Pickles, Lettuce & Cheese",
-                    "wrap": true,
-                    "color": "#aaaaaa",
-                    "size": "xxs"
-                  }
-                ]
-              },
-              "footer": {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
-                  {
-                    "type": "spacer",
-                    "size": "xxl"
-                  },
-                  {
-                    "type": "button",
-                    "style": "primary",
-                    "color": "#905c44",
-                    "action": {
-                      "type": "message",
-                      "label": "Order",
-                      "text": "CM01_001"
-                
-		    }
-              
-		  }
-            
-		]
-         
-	      }
-	 }
+  "type": "bubble",
+  "header": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Header text"
+      }
+    ]
+  },
+  "hero": {
+    "type": "image",
+    "url": "https://drive.google.com/uc?export=download&id=1KNgmkrvog0OnUHRil9seI8CfyVAWtn3j",
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Body text",
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Footer text",
+      }
+    ]
+  },
+  "styles": {
+    "comment": "test"
+  }
 };
 
 function joke() {
