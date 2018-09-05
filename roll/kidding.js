14bdde80b8f049dea8bf3d9318be7b9b867c14d8
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-09-05 23:23:48 +0800

    Update kidding.js

--- roll/kidding.js
+++ roll/kidding.js
@@ -3,44 +3,25 @@ var funny = require('./funny.js');
 var rply = { type: 'text' }; //type是必需的,但可以更改
 
 function joke() {
-	rply = {
-  "type": "bubble",
-  "header": {
-    "type": "box",
-    "layout": "vertical",
-    "contents": [
-      {
-        "type": "text",
-        "text": "Header text"
-      }
-    ]
-  },
-  "hero": {
-    "type": "image",
-    "url": "https://drive.google.com/uc?export=download&id=1KNgmkrvog0OnUHRil9seI8CfyVAWtn3j",
-  },
-  "body": {
-    "type": "box",
-    "layout": "vertical",
-    "contents": [
-      {
-        "type": "text",
-        "text": "Body text",
-      }
-    ]
-  },
-  "footer": {
-    "type": "box",
-    "layout": "vertical",
-    "contents": [
-      {
-        "type": "text",
-        "text": "Footer text",
-      }
-    ]
-  },
-  "styles": {
-    "comment": "See the example of a bubble style object"
+	rply = {  
+  "type": "flex",
+  "altText": "this is a flex message",
+  "contents": {
+    "type": "bubble",
+    "body": {
+      "type": "box",
+      "layout": "vertical",
+      "contents": [
+        {
+          "type": "text",
+          "text": "hello"
+        },
+        {
+          "type": "text",
+          "text": "world"
+        }
+      ]
+    }
   }
 };
 	return rply;
