3ab850d9939878bf3c5087ab4c6ad4236756e499
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-09-06 00:16:57 +0800

    Update kidding.js

--- roll/kidding.js
+++ roll/kidding.js
@@ -1,41 +1,63 @@
 var rollbase = require('./rollbase.js');
 var funny = require('./funny.js');
 var message = {
-  "type": "bubble",
-  "header": {
-    "type": "box",
-    "layout": "vertical",
-    "contents": [
-      {
-        "type": "text",
-        "text": "Header text"
-      }
-    ]
-  },
-  "hero": {
-    "type": "image",
-    "url": "https://drive.google.com/uc?export=download&id=1KNgmkrvog0OnUHRil9seI8CfyVAWtn3j",
-  },
-  "body": {
-    "type": "box",
-    "layout": "vertical",
-    "contents": [
-      {
-        "type": "text",
-        "text": "Body text",
-      }
-    ]
-  },
-  "footer": {
-    "type": "box",
-    "layout": "vertical",
-    "contents": [
-      {
-        "type": "text",
-        "text": "Footer text",
-      }
-    ]
-  }
+    "type": "flex",
+    "altText": "#",
+    "contents": {
+        "type": "bubble",
+        "hero": {
+            "type": "image",
+            "url": "https://s3-ap-northeast-1.amazonaws.com/xxxxxxx/hamburger.jpg",
+            "size": "full",
+            "aspectRatio": "20:13",
+            "aspectMode": "cover",
+            "action": {
+                "type": "uri",
+                "label": "#",
+                "uri": "https://classmethod.jp/"
+            }
+        },
+        "body": {
+            "type": "box",
+            "layout": "vertical",
+            "spacing": "md",
+            "contents": [
+            {
+                "type": "text",
+                "text": "Hamburger",
+                "wrap": true,
+                "weight": "bold",
+                "gravity": "center",
+                "size": "xl"
+            },
+            {
+                "type": "box",
+                "layout": "vertical",
+                "margin": "xxl",
+                "contents": [
+                {
+                    "type": "spacer",
+                    "size": "lg"
+                },
+                {
+                    "type": "image",
+                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/linecorp_code_withborder.png",
+                    "aspectMode": "cover",
+                    "size": "xl"
+                },
+                {
+                    "type": "text",
+                    "text": "You can pick up your order by using this code instead of a ticket.",
+                    "color": "#aaaaaa",
+                    "wrap": true,
+                    "margin": "xxl",
+                    "size": "xs"
+                }
+                ]
+            }
+            ]
+        }
+    }
 };
 
 function joke() {
