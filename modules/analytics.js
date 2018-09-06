a082b70417468461d812f4371deb68a103d82fe3
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-09-06 14:04:27 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -86,9 +86,9 @@ function parseInput(rplyToken, inputStr) {
 	if (trigger.match(/^suggestcard$/)!= null ) return exports.cardsuggest.Suggestion();
 	
 	if (trigger.match(/（）/) != null) {
-		if (trigger.match(/^亞瑟/)!= null ) return exports.cardbuild.build();
-		if (trigger.match(/^妖精龍/)!= null ) return exports.cardbuild.build1();
-		if (trigger.match(/^ttt/)!= null ) return exports.cardbuild.build2();
+		if (trigger.match(/^亞瑟/)!= null ) return exports.cardbuild.royal();
+		if (trigger.match(/^妖精龍/)!= null ) return exports.cardbuild.elfdragon();
+		if (trigger.match(/^星爆猴/)!= null ) return exports.cardbuild.build2();
 		if (trigger.match(/^護符/)!= null ) return exports.cardbuild.build3();
 	}
 
