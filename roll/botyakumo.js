214954d15bee44ad43c8d814db3b260a3ab397ab
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-09-06 16:21:17 +0800

    Update botyakumo.js

--- roll/botyakumo.js
+++ roll/botyakumo.js
@@ -202,59 +202,8 @@ var bothelpone = {
 							"label": "【車圖整合(慎用)】",
 							"text": "comfirephoto"
 						}
-
-					]
-				}
-			]
-		}	
-};
-   
-var bothelptwo = {
-		"type": "template",
-		"altText": "【式神的教學2】",
-		"template": {
-			"type": "carousel",
-			"columns": [
-				{
-					"title": "隨機生成/其他",
-					"text": "隨機生成/其他",
-					"actions": [
-						{
-							"type": "message",
-							"label": "幸運顏色",
-							"text": "幸運顏色"
-						},
-						{
-							"type": "message",
-							"label": "撲克",
-							"text": "撲克1-14/撲克大老二\n\n用法自行發掘，最高抽撲克功能只支援到14張。\n\n【例子1】撲克12\n\n【例子2】撲克大老二"
-						},
-						{
-							"type": "message",
-							"label": "偽式神幫助【毀其他人形象用】",
-							"text": "式神幫忙"
-						}
-					]
-				},
-				{
-					"title": "其他功能",
-					"text": "其他",
-					"actions": [
-						{
-							"type": "message",
-							"label": "推薦牌組列表",
-							"text": "comfirmcards"
-						},
-						{
-							"type": "message",
-							"label": "暫無",
-							"text": "暫無"
-						},
-						{
-							"type": "message",
-							"label": "暫無",
-							"text": "暫無"
-						}				
+						
+						
 					]
 				}
 			]
