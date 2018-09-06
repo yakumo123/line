2f10a50db32a349f956ad468594cff8df06e0781
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-09-07 00:51:53 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -87,9 +87,8 @@ function parseInput(rplyToken, inputStr) {
 		if (trigger.match(/^幽靈/)!= null ) return exports.cardbuild.onetea();
 		if (trigger.match(/^葬送/)!= null ) return exports.cardbuild.twotea();
 		if (trigger.match(/^騙水吸/)!= null ) return exports.cardbuild.vampilim();
-
-
-	
+		if (trigger.match(/^殺戮土/)!= null ) return exports.cardbuild.vampilim();
+		
 	}
 	
 	if (trigger.match(/^問號$/)!= null ) return exports.kiddingme.joking();
