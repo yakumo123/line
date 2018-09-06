e3182e5e54fac93cc79c4154b360195eb6a84d61
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-09-06 12:56:04 +0800

    Update testing.js

--- roll/testing.js
+++ roll/testing.js
@@ -5,6 +5,7 @@ var rply = { type: 'text' }; //type是必需的,但可以更改
 function Test12() {
 	rply = {
   "type": "template",
+"altText": "【警告】",
   "template": {
       "type": "confirm",
       "text": "【警告】本列表含有大量訊息 請勿在【群組】內使用 請使用【私訊】 \n \n本Bot作者因方便其使用者使用本Bot而設立本指令 其不當使用/不跟隨以上警告所使用 所有責任不應歸其作者身上 其所有責任均在其使用者身上 本Bot作者不會對其所發生的事情負上一切責任 \n \n確定使用?",
@@ -28,6 +29,7 @@ function Test12() {
 function Test13() {
 	rply = {
   "type": "template",
+		"altText": "【警告】",
   "template": {
       "type": "confirm",
       "text": "【警告】本列表含有大量訊息 請盡量不要在【群組】內使用 請使用【私訊】 \n \n本Bot作者因方便其使用者使用本Bot而設立本指令 其不當使用/不跟隨以上警告所使用 所有責任不應歸其作者身上 其所有責任均在其使用者身上 本Bot作者不會對其所發生的事情負上一切責任 \n \n確定使用?",
