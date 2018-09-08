33b1f07fb480876643bab97a8a156301f3d02ec2
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-09-08 11:36:32 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -397,6 +397,9 @@ if (trigger.match(/無限制7張虹卡禁卡|無限制7張虹卡限卡|無限制
 	
 	if (trigger.match(/知道嗎/) != null) return exports.funny.know() ;	
 
+	if (trigger.match(/基德/) != null) return exports.kidding.inin() ;	
+
+	
 	if (trigger.match(/猜拳/) != null) {
 		return RockPaperScissors(inputStr, mainMsg[1]);
 	}
