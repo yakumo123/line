10f9509564f1817c7807860242783beb09ae9ffb
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-09-15 01:22:12 +0800

    Update botyakumo2.js

--- roll/botyakumo2.js
+++ roll/botyakumo2.js
@@ -15,29 +15,8 @@ function BotHelpYy() {
 					"actions": [
 						{
 							"type": "message",
-							"label": "幸運顏色",
-							"text": "幸運顏色"
-						},
-						{
-							"type": "message",
-							"label": "撲克",
-							"text": "撲克1-14/撲克大老二\n\n用法自行發掘，最高抽撲克功能只支援到14張。\n\n【例子1】撲克12\n\n【例子2】撲克大老二"
-						},
-						{
-							"type": "message",
-							"label": "偽式神幫助【毀其他人形象用】",
-							"text": "式神幫忙"
-						}
-					]
-				},
-				{
-					"title": "其他功能",
-					"text": "其他",
-					"actions": [
-						{
-							"type": "message",
-							"label": "推薦牌組列表",
-							"text": "comfirmcards"
+							"label": "生成隨機名字",
+							"text": "生成名字"
 						},
 						{
 							"type": "message",
