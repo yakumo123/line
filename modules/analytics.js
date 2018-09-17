2a14e2fa5dd534f90e83cb4992c07784d338573e
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-09-17 23:09:16 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -226,7 +226,7 @@ function parseInput(rplyToken, inputStr) {
 		
 	if (trigger.match(/(渦芽（橙靈心）$)|^渦芽$/) != null) return exports.funny.rrrr() ;	
 	
-	if (trigger.match(/^紅茶$|^@紅茶$/) != null) return exports.funny.tea() ;	
+	if (trigger.match(/^紅茶$/) != null) return exports.funny.tea() ;	
 	
 	if (trigger.match(/知道嗎/) != null) return exports.funny.know() ;	
 
