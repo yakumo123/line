20e24358b92125985031d52d8176514d250eb6ff
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-09-22 15:50:14 +0800

    Update fakeimage.js

--- roll/fakeimage.js
+++ roll/fakeimage.js
@@ -7,28 +7,17 @@ var rply = {
 				"type": "image",
 				"originalContentUrl": "https://drive.google.com/uc?export=download&id=1OXQlKRyPzDr2jWUiZrGaTjY8k59AVJuJ",
 				"previewImageUrl": "https://drive.google.com/uc?export=download&id=1WoW_uvAtWepgtL3BWtYxL6SCkeHjTEXw"		
-	},
-    {
+		},
+		{
 				"type": "image",
 				"originalContentUrl": "https://drive.google.com/uc?export=download&id=1OXQlKRyPzDr2jWUiZrGaTjY8k59AVJuJ",
 				"previewImageUrl": "https://drive.google.com/uc?export=download&id=1WoW_uvAtWepgtL3BWtYxL6SCkeHjTEXw"		
-	},
-    {
+		},
+		{
 				"type": "image",
 				"originalContentUrl": "https://drive.google.com/uc?export=download&id=1OXQlKRyPzDr2jWUiZrGaTjY8k59AVJuJ",
 				"previewImageUrl": "https://drive.google.com/uc?export=download&id=1WoW_uvAtWepgtL3BWtYxL6SCkeHjTEXw"		
-	},
-    {
-				"type": "image",
-				"originalContentUrl": "https://drive.google.com/uc?export=download&id=1OXQlKRyPzDr2jWUiZrGaTjY8k59AVJuJ",
-				"previewImageUrl": "https://drive.google.com/uc?export=download&id=1WoW_uvAtWepgtL3BWtYxL6SCkeHjTEXw"		
-	},
-    {
-				"type": "image",
-				"originalContentUrl": "https://drive.google.com/uc?export=download&id=1OXQlKRyPzDr2jWUiZrGaTjY8k59AVJuJ",
-				"previewImageUrl": "https://drive.google.com/uc?export=download&id=1WoW_uvAtWepgtL3BWtYxL6SCkeHjTEXw"		
-	
-    }	
+		}
 	]
 };
 
