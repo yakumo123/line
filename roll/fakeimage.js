8b6eb99344aa2089fce24beb29fd23006214a0e1
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-09-22 16:09:34 +0800

    Update fakeimage.js

--- roll/fakeimage.js
+++ roll/fakeimage.js
@@ -2,24 +2,10 @@ var rollbase = require('./rollbase.js');
 var funny = require('./funny.js');
 
 var rply = {
-	"messages":[
-		{
 				"type": "image",
 				"originalContentUrl": "https://drive.google.com/uc?export=download&id=1OXQlKRyPzDr2jWUiZrGaTjY8k59AVJuJ",
-				"previewImageUrl": "https://drive.google.com/uc?export=download&id=1WoW_uvAtWepgtL3BWtYxL6SCkeHjTEXw"		
-		},
-		{
-				"type": "image",
-				"originalContentUrl": "https://drive.google.com/uc?export=download&id=1OXQlKRyPzDr2jWUiZrGaTjY8k59AVJuJ",
-				"previewImageUrl": "https://drive.google.com/uc?export=download&id=1WoW_uvAtWepgtL3BWtYxL6SCkeHjTEXw"		
-		},
-		{
-				"type": "image",
-				"originalContentUrl": "https://drive.google.com/uc?export=download&id=1OXQlKRyPzDr2jWUiZrGaTjY8k59AVJuJ",
-				"previewImageUrl": "https://drive.google.com/uc?export=download&id=1WoW_uvAtWepgtL3BWtYxL6SCkeHjTEXw"		
-		}
-	]
-};
+				"previewImageUrl": "https://drive.google.com/uc?export=download&id=1WoW_uvAtWepgtL3BWtYxL6SCkeHjTEXw"
+}
 
 
 function fake() {
