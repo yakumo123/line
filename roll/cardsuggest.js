77feed16f3ae3716c019ad312c1e2bca72f62d08
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-10-14 22:23:24 +0800

    Update cardsuggest.js

--- roll/cardsuggest.js
+++ roll/cardsuggest.js
@@ -74,6 +74,28 @@ function Suggestion() {
 							"text": "八雲唯我皇☆推薦"
 						}
 					]
+				},
+				{
+					"title": "推薦牌組列表【接受投稿】",
+					"text": "推薦牌組列表",
+					"actions": [
+						{
+							"type": "message",
+							"label": "阿卡斯死-賤BB【指定】",
+							"text": "阿卡☆推薦"
+						},
+						{
+							"type": "message",
+							"label": "寶藏亞瑟皇-shermanLai【指定】",
+							"text": "寶藏☆推薦"
+							
+						},
+						{
+							"type": "message",
+							"label": "暫無",
+							"text": "暫無"
+						}
+					]
 				}
 			]
 		}
