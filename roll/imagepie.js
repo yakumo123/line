e3f1a109b46763e2165b20238058c46780ffd1f2
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2018-11-10 13:46:46 +0800

    Update imagepie.js

--- roll/imagepie.js
+++ roll/imagepie.js
@@ -5,8 +5,8 @@ var rply = { type: 'text' }; //type是必需的,但可以更改
 function PieIm() {
 	rply = {
     "type": "image",
-    "originalContentUrl": "https://drive.google.com/uc?export=download&id=1NXC7gY9CMKp5UbduwhPvFlkry7mYMQq2",
-    "previewImageUrl": "https://drive.google.com/uc?export=download&id=1NXC7gY9CMKp5UbduwhPvFlkry7mYMQq2"
+    "originalContentUrl": "https://i.imgur.com/a8kqqYn.jpg",
+    "previewImageUrl": "https://i.imgur.com/a8kqqYn.jpg"
 };
 	return rply;
 }
