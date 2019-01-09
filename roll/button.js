eb649ffc9651708ae0c154173e3ad2e1f1b602ab
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2019-01-09 23:42:12 +0800

    Update button.js

--- roll/button.js
+++ roll/button.js
@@ -12,7 +12,7 @@ function buttontest() {
     "imageSize": "cover",
     "columns": [
       {
-        "thumbnailImageUrl": "https://shadowverse.jp/news/wp-content/uploads/e0b558ba389c34594d109581059d77f1.png",
+        "thumbnailImageUrl": "https://i2.wp.com/sequentialplanet.com/wp-content/uploads/2018/12/ogp.png?fit=646%2C269&ssl=1",
         "imageBackgroundColor": "#a8e8fb",
         "title": "SV活動情報及ShadowLogs",
         "text": "SV活動情報及ShadowLogs",
@@ -24,8 +24,8 @@ function buttontest() {
 			      },
 			      {
 				      "type": "uri",
-				      "label": "追加卡包情報 【感謝LM.緯提供】",
-				      "uri": "https://m.gamer.com.tw/forum/C.php?bsn=29330&snA=15691&bpage=1&top=9&ltype="
+				      "label": "Shadowverse News",
+				      "uri": "https://shadowverse.com/sp/cht/news/"
 			      },	
 			      {
 				      "type": "uri",				      
@@ -35,7 +35,7 @@ function buttontest() {
         ]
       },
       {
-        "thumbnailImageUrl": "https://i.ytimg.com/vi/iYRO-R_fRq4/maxresdefault.jpg",
+        "thumbnailImageUrl": "https://i2.wp.com/sequentialplanet.com/wp-content/uploads/2018/12/ogp.png?fit=646%2C269&ssl=1",
         "imageBackgroundColor": "#a8e8fb",
 	      "title": "競技場牌組",
         "text": "競技場牌組",
@@ -59,25 +59,25 @@ function buttontest() {
         ]
       },
       {
-        "thumbnailImageUrl": "https://shadowverse.gamepress.gg/sites/shadowverse/files/2018-09/Omen-of-the-Ten_2.png",
+        "thumbnailImageUrl": "https://i2.wp.com/sequentialplanet.com/wp-content/uploads/2018/12/ogp.png?fit=646%2C269&ssl=1",
         "imageBackgroundColor": "#a8e8fb",
         "title": "投稿推薦牌組",
         "text": "內容較多，群組內慎用以免做成洗頻。",
         "actions": [
           {
-   "type": "message",
-                "label": "投稿推薦牌組10月版",
-                "text": "sv推薦牌組10月"
+   "type": "postback",
+                "label": "投稿推薦牌組1月版【待更】",
+                "data": "sv推薦牌組1月"
             },
             {
                 "type": "postback",
-                "label": "投稿推薦牌組11月版【待更】",
+                "label": "投稿推薦牌組2月版【待更】",
                 "data": "待更新"
             },	      
 
 		{
                 "type": "postback",
-                "label": "投稿推薦牌組12月版【待更】",
+                "label": "投稿推薦牌組3月版【待更】",
                 "data": "待更新"
             }
         ]
