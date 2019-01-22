09b2165d0228bcdf63578e65f271bd085fe44934
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2019-01-22 19:17:24 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -142,7 +142,7 @@ function parseInput(rplyToken, inputStr) {
 		if (trigger.match(/^大十字|^cross/)!= null) return exports.funny.MultiDrawTarot(mainMsg[1], mainMsg[2], 2);
 	}
 	
-	if (trigger.match(/你就爛|你就是爛/)!= null ) return exports.funny.suck(mainMsg);
+	if (trigger.match(/就爛|就是爛|你就爛|你就是爛/)!= null ) return exports.funny.suck(mainMsg);
 
 	if (trigger.match(/他就爛|他就是爛|她就是爛|她就爛/)!= null ) return exports.funny.suck(mainMsg);
 
