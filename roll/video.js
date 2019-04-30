ca2b25565ac8a703971e37787846e41c77f83fa8
lineyakubot <42207454+lineyakubot@users.noreply.github.com>
2019-04-30 16:11:32 +0800

    Update video.js

--- roll/video.js
+++ roll/video.js
@@ -3,13 +3,14 @@ var funny = require('./funny.js');
 var rply = { type: 'text' }; //type是必需的,但可以更改
 
 function mushroomcloud() {
-	rply = {
-    "type": "audio",
-    "duration": "6000",
- "contentProvider": {
-      "type": "external",
-    "contentProvider.originalContentUrl": "https://drive.google.com/uc?export=download&id=1bZC81tfqPLrbk7tySsieI30YYwlFVrWM"
- }
+	rply = { 
+		"type": "audio",
+		"duration": 60000,
+		"contentProvider": {
+			"type": "external",
+			"contentProvider.originalContentUrl":
+			"https://drive.google.com/uc?export=download&id=1bZC81tfqPLrbk7tySsieI30YYwlFVrWM"
+		}
 	};
 	return rply;
 }
