1c8188c6c836b286bd048dc306d55962c263ea13
yakumo123 <52350839+yakumo123@users.noreply.github.com>
2020-05-28 20:59:17 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -59,7 +59,8 @@ function parseInput(rplyToken, inputStr) {
 
 	if (trigger.match(/紅茶的綜合分數$|紅茶綜合分數$/) != null) return exports.teachar.ability(mainMsg) ;	
 
-	
+	if (trigger.match(/配對$/) != null) return exports.funny.randomMatch(mainMsg) ;	
+
 	if (trigger.match(/綜合分數$/) != null) return exports.character.ability(mainMsg) ;	
 
 	if (trigger.match(/^生成卡片$|^卡片生成$|^生成卡牌$|^卡牌生成$/) != null && mainMsg[1] != NaN )	return exports.createcard.build7char(mainMsg[1]);
